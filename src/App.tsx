5aa6792f72e77c1348483f769c9640ae44b7ec7c
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-07-22 12:24:22 +0900

    feat : 불필요한 코드 제거 #1

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { useRoutes } from 'react-router-dom';
 import { routes } from '@router';
 
 const App = () => {
-  console.log(routes);
-
   const element = useRoutes(routes);
 
   return <div>{element}</div>;
