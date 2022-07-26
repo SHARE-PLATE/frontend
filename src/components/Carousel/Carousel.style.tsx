57ac60d4d710f3886a5111f725d5b427c77b7227
JinJeon <nanyong01@gmail.com>
2022-07-26 17:56:25 +0900

    feat : Carousel 컴포넌트 생성

---
+++ src/components/Carousel/Carousel.style.tsx
@@ -0,0 +1,64 @@
+import Slider from 'react-slick';
+import styled, { css } from 'styled-components';
+
+export const CarouselSlider = styled(Slider)`
+  height: 100%;
+  width: 100%;
+
+  .slick-dots {
+    // carousel 하단 점 아이콘 설정
+    bottom: 0.5rem;
+
+    button {
+      ::before {
+        opacity: 0.5;
+        color: black;
+      }
+    }
+
+    .slick-active {
+      button {
+        ::before {
+          color: black;
+        }
+      }
+    }
+  }
+
+  .slick-list,
+  .slick-track,
+  .slick-slide {
+    height: 100%;
+  }
+
+  .slick-slide {
+    > div {
+      height: 100%;
+    }
+  }
+
+  .slick-prev,
+  .slick-next {
+    z-index: 2;
+    ::before {
+      opacity: 0.5;
+      color: black;
+    }
+  }
+
+  .slick-prev {
+    left: 1rem;
+  }
+
+  .slick-next {
+    right: 1rem;
+  }
+`;
+
+export const CarouselWrapper = styled.div<{ width: number; height: number }>`
+  ${({ width, height }) => css`
+    width: ${width}%;
+    height: ${height}%;
+    overflow: hidden;
+  `}
+`;
