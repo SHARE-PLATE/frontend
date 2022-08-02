1ef60cefdd5a424bfbd8f97160a035cf035b5250
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-08-02 11:40:11 +0900

    feat : 텍스트 -> 아이콘으로 변경 #19

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,29 @@
 import styled from 'styled-components';
 
 import Address from '@components/Address';
-import LoginButton from '@components/LoginButton';
-import SideBar from '@components/SideBar';
-import { Pc } from '@query/mediaQuery';
+import Icon from '@components/common/Icon';
+import { ICON_NAME, ICON_SIZE } from '@components/common/Icon/constants';
+import { flexBetween } from '@styles/mixin';
 
 const Header = () => {
   return (
     <Wrapper>
       <div>
-        <span>logo</span>
+        <Icon iconName={ICON_NAME.LOGO} iconSize={ICON_SIZE.LARGE} />
       </div>
       <div>
         <Address />
       </div>
       <div>
-        <span>알림 </span>
-        <LoginButton />
-        <Pc>
-          <SideBar />
-        </Pc>
+        <Icon iconName={ICON_NAME.NOTICE} iconSize={ICON_SIZE.LARGE} />
       </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  ${flexBetween}
+  height: 40px;
 `;
 
 export default Header;
