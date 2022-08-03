62902beb1510a7c754a00699399cdf3bef1668e2
JinJeon <nanyong01@gmail.com>
2022-08-03 16:57:05 +0900

    feat : SearchRecent의 하부 요소 클릭 시 검색 입력 기능 동작하도록 설정 #24

--- src/components/SearchRecent/SearchRecent.style.tsx
+++ src/components/SearchRecent/SearchRecent.style.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const RecentWrapper = styled.div`
   flex-grow: 1;
@@ -14,14 +14,18 @@ export const NoRecentListWrapper = styled.div`
 `;
 
 export const RecentHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  font-weight: 700;
+  ${({ theme: { fonts } }) => css`
+    ${fonts.xLargeBold};
+    display: flex;
+    justify-content: space-between;
+  `}
 `;
 
 export const RecentDeleteAllBtn = styled.button`
-  color: grey;
-  font-size: 0.8rem;
+  ${({ theme: { fonts } }) => css`
+    ${fonts.medium};
+    color: #999999;
+  `}
 `;
 
 export const RecentListWrapper = styled.div`
@@ -38,11 +42,35 @@ export const RecentItemWrapper = styled.div`
 `;
 
 export const RecentItemInfo = styled.div`
+  ${({ theme: { fonts } }) => css`
+    ${fonts.medium}
+  `}
+
   display: flex;
   justify-content: space-between;
   flex-grow: 1;
 `;
 
+export const RecentItemName = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    padding-top: 0.25rem;
+  }
+`;
+
+export const RecentItemDate = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 0.2rem;
+  color: #999999;
+`;
+
 export const RecentDeleteBtn = styled.button`
   display: flex;
+  align-items: center;
 `;
