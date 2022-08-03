c504225832a8acbe1f3e1ed5270135d43e5d17d3
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-08-03 16:17:53 +0900

    feat : 필터 정보 추가 #22

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,12 +1,12 @@
-interface categoryItemType {
+export interface categoryItemType {
   id: number;
   name: string;
-  link: string;
+  filter: string;
 }
 
 export const categoryItem: categoryItemType[] = [
-  { id: 1, name: '가격순', link: '필터' },
-  { id: 2, name: '거리순', link: '필터' },
-  { id: 3, name: '최신순', link: '필터' },
-  { id: 4, name: '마감임박', link: '필터' },
+  { id: 1, name: '가격순', filter: 'price' },
+  { id: 2, name: '거리순', filter: 'distance' },
+  { id: 3, name: '최신순', filter: 'recency' },
+  { id: 4, name: '마감임박', filter: 'deadline' },
 ];
