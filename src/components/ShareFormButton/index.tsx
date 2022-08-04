0489d043d41b75e2885daae5ba6978d7b086790a
JinJeon <nanyong01@gmail.com>
2022-08-04 12:03:04 +0900

    feat : share form 페이지에 있을 시 share form button 보이지 않도록 설정 #24

--- src/components/ShareFormButton/index.tsx
+++ src/components/ShareFormButton/index.tsx
@@ -1,14 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { Wrapper } from '@components/ShareFormButton/ShareFormButton.style';
 import Icon from '@components/common/Icon';
 
+const shareFormLink = '/share-form';
+
 const ShareFormButton = () => {
+  const { pathname } = useLocation();
+
   return (
-    <Link to='share-form'>
-      <Wrapper>
-        <Icon iconName='Form' iconSize='MEDIUM' />
-      </Wrapper>
+    <Link to={shareFormLink}>
+      {pathname !== shareFormLink && (
+        <Wrapper>
+          <Icon iconName='Form' iconSize='MEDIUM' />
+        </Wrapper>
+      )}
     </Link>
   );
 };
