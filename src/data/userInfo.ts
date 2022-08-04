3b7894ecea049026e67b596d80a806d41d8abb6b
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-08-04 11:54:02 +0900

    feat : 유저 정보 목 데이터 #27

---
+++ src/data/userInfo.ts
@@ -0,0 +1,11 @@
+interface userInfoExampleType {
+  profileImageUrl: string;
+  nickname: string;
+  email: string;
+}
+
+export const userInfoExample: userInfoExampleType = {
+  profileImageUrl: 'https://avatars.githubusercontent.com/u/93566353?v=4',
+  nickname: 'kim',
+  email: 'HJKL2022@gmail.com',
+};
