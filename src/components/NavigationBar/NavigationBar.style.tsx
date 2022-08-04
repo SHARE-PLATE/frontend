cab05e9fd075f8a999ba7d910c39fb705d77ba9d
JinJeon <nanyong01@gmail.com>
2022-08-04 23:14:08 +0900

    style : Navigation Bar 디자인 적용

--- src/components/NavigationBar/NavigationBar.style.tsx
+++ src/components/NavigationBar/NavigationBar.style.tsx
@@ -1,13 +1,14 @@
 import styled, { css } from 'styled-components';
 
 export const NavigationBarWrapper = styled.div`
-  ${({ theme: { defaultWidth } }) => css`
+  ${({ theme: { defaultWidth, colors } }) => css`
     position: fixed;
     display: flex;
     justify-content: space-around;
     padding: 0.4rem;
-    color: white;
-    background-color: black;
+    color: ${colors.grey4};
+    background-color: ${colors.white1};
+    box-shadow: 0px -10px 40px #67676740;
     width: 100%;
     ${defaultWidth};
     height: 7%;
@@ -16,10 +17,22 @@ export const NavigationBarWrapper = styled.div`
   `}
 `;
 
-export const NavigationBarBtn = styled.button`
-  color: white;
+export const NavigationBarBtn = styled.button<{ isSelected: boolean }>`
+  ${({ theme: { colors, fonts }, isSelected }) => css`
+    color: ${colors.grey4};
+    ${fonts.small};
+    ${isSelected &&
+    css`
+      color: ${colors.black};
+      path {
+        stroke: ${colors.black};
+      }
+    `}
+  `}
+
   display: flex;
-  padding: 1rem;
+  flex-grow: 1;
   flex-direction: column;
+  align-items: center;
   justify-content: center;
 `;
