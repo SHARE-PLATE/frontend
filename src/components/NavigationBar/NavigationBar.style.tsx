ae0e8ad99c7b0f784f465bac18b566d923601fd5
JinJeon <nanyong01@gmail.com>
2022-08-04 17:06:44 +0900

    style : navigation bar, search form button 디자인 적용 방식 변경 #30

--- src/components/NavigationBar/NavigationBar.style.tsx
+++ src/components/NavigationBar/NavigationBar.style.tsx
@@ -1,20 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const NavigationBarWrapper = styled.div`
-  z-index: 3;
-  display: flex;
-  justify-content: space-around;
-  padding: 0.4rem;
-  color: white;
-  background-color: black;
-  width: 100%;
-  max-width: 750px;
-  aspect-ratio: 100 / 15;
-  max-height: 60px;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
+  ${({ theme: { defaultWidth } }) => css`
+    position: fixed;
+    display: flex;
+    justify-content: space-around;
+    padding: 0.4rem;
+    color: white;
+    background-color: black;
+    width: 100%;
+    ${defaultWidth};
+    height: 7%;
+    max-height: 60px;
+    bottom: 0;
+  `}
 `;
 
 export const NavigationBarBtn = styled.button`
