4f0e1c83957d06a28bbc191d52d7e542a79ba46e
JinJeon <nanyong01@gmail.com>
2022-08-04 12:12:24 +0900

    refactor : search popular 내의 상수들 선언 위치 변경

--- src/components/SearchPopular/index.tsx
+++ src/components/SearchPopular/index.tsx
@@ -1,15 +1,8 @@
 import { FormEvent } from 'react';
 
 import * as S from '@components/SearchPopular/SearchPopular.style';
-import { popularKeyword } from '@constants/mentions';
-
-const popularKeywordsInfo = [
-  { id: 0, name: '고구마' },
-  { id: 1, name: '치킨' },
-  { id: 2, name: '떡볶이' },
-  { id: 3, name: '당근' },
-  { id: 4, name: '파프리카' },
-];
+import { popularKeywordMention } from '@constants/mentions';
+import { popularKeywordsInfo } from '@constants/popularKeywords';
 
 type SearchPopularPropsType = {
   clickHandler: (event: FormEvent | string) => void;
@@ -25,7 +18,7 @@ const SearchPopular = ({ clickHandler }: SearchPopularPropsType) => {
 
   return (
     <S.Wrapper>
-      <S.Header>{popularKeyword}</S.Header>
+      <S.Header>{popularKeywordMention}</S.Header>
       <S.Contents>{popularKeywordsContents}</S.Contents>
     </S.Wrapper>
   );
--- src/constants/mentions.ts
+++ src/constants/mentions.ts
@@ -2,4 +2,4 @@ export const loginMention = '카카오로 로그인하고\n여러 사람들과 �
 export const kakaoLoginMention = '카카오로 로그인';
 export const noRecentListMention = '최근 검색 기록이 없습니다';
 export const inputKeyword = '검색어를 입력하세요';
-export const popularKeyword = '인기있는 쉐어플레이트';
+export const popularKeywordMention = '인기있는 쉐어플레이트';
