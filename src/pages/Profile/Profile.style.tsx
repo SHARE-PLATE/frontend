4e3d0fa8c08f40169348a68b97e74491a8383e98
JinJeon <nanyong01@gmail.com>
2022-08-05 16:32:10 +0900

    style : Profile 부분 스타일 수정

---
+++ src/pages/Profile/Profile.style.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+import { defaultPageStyle } from '@styles/mixin';
+
+export const ProfileWrapper = styled.div`
+  ${defaultPageStyle};
+`;
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -3,16 +3,19 @@ import ProfileHeader from '@components/ProfileHeader';
 import ReserveShare from '@components/ReserveShare';
 import UserInfo from '@components/UserInfo';
 import UserMenu from '@components/UserMenu';
+import * as S from '@pages/Profile/Profile.style';
 
 const Profile = () => {
   return (
-    <>
-      <ProfileHeader />
-      <UserInfo />
-      <MyMenu />
+    <S.ProfileWrapper>
+      <div>
+        <ProfileHeader />
+        <UserInfo />
+        <MyMenu />
+      </div>
       <ReserveShare />
       <UserMenu />
-    </>
+    </S.ProfileWrapper>
   );
 };
 
