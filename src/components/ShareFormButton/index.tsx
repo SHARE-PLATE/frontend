1d0b93adcc0c0fdaabbc586bc37a35a6766f7029
JinJeon <nanyong01@gmail.com>
2022-08-05 17:02:04 +0900

    feat : share form button이 profile 페이지에서도 숨겨지도록 설정

--- src/components/ShareFormButton/index.tsx
+++ src/components/ShareFormButton/index.tsx
@@ -2,16 +2,17 @@ import { useNavigate, useLocation } from 'react-router-dom';
 
 import { Wrapper } from '@components/ShareFormButton/ShareFormButton.style';
 import Icon from '@components/common/Icon';
+import { pathName } from '@router';
 
-const shareFormLink = '/share-form';
+const { shareForm, profile } = pathName;
 
 const ShareFormButton = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  const isShareForm = pathname === shareFormLink;
+  const isShareForm = pathname === '/' + shareForm || pathname === '/' + profile;
 
   return (
-    <Wrapper onClick={() => navigate(shareFormLink)} isShareForm={isShareForm}>
+    <Wrapper onClick={() => navigate(shareForm)} isShareForm={isShareForm}>
       <Icon iconName='Form' iconSize='MEDIUM' />
     </Wrapper>
   );
