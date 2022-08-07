5895dad6b6867b91da63a7c80d06c891c8ac4711
JinJeon <nanyong01@gmail.com>
2022-08-07 11:33:57 +0900

    feat : 긴 이미지 형식의 프리뷰 아이템(ShareListItemLongImage) 컴포넌트 추가

---
+++ src/components/ShareListItemLongImage/index.tsx
@@ -0,0 +1,28 @@
+import { RemainedTime } from '@components/RemainedTime';
+import * as S from '@components/ShareListItemLongImage/ShareListItemLongImage.style';
+import { listExampleType } from '@data/shareList';
+import { getPriceType } from '@utils/getPriceType';
+import { getTimeDiffInHour } from '@utils/getTimeDiff';
+
+export const ShareListItemLongImage = ({ itemInfo }: { itemInfo: listExampleType }) => {
+  const { title, location, price, originalPrice, thumbnailUrl, appointmentDateTime } = itemInfo;
+  const timeDiff = getTimeDiffInHour(appointmentDateTime);
+  if (timeDiff === 'over' || timeDiff === 'done') return null;
+
+  return (
+    <S.ItemWrapper>
+      <RemainedTime targetTime={appointmentDateTime} />
+      <S.ImgWrapper>
+        <S.StyledImg src={thumbnailUrl} alt={title} />
+      </S.ImgWrapper>
+      <S.InfoWrapper>
+        <S.ItemTitle>{title}</S.ItemTitle>
+        <S.ItemLocation>{`${location} ∙ 몇 시간 전`}</S.ItemLocation>
+        <S.ItemPrice>
+          <div>{getPriceType({ price, isUnit: true })}</div>
+          <div>{getPriceType({ price: originalPrice, isUnit: true })}</div>
+        </S.ItemPrice>
+      </S.InfoWrapper>
+    </S.ItemWrapper>
+  );
+};
