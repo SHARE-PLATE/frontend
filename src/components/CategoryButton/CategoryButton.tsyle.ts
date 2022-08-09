0c62437a37d71810201f90fd8db52dc2a128c069
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-08-09 12:00:32 +0900

    feat : 카테고리 버튼 스타일 분리 #40

---
+++ src/components/CategoryButton/CategoryButton.tsyle.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.ul`
+  display: flex;
+  padding: 0 1rem;
+`;
+
+export const Item = styled.li``;
--- src/components/CategoryButton/index.tsx
+++ src/components/CategoryButton/index.tsx
@@ -1,28 +1,32 @@
 import { useSetRecoilState } from 'recoil';
 
+import * as S from '@components/CategoryButton/CategoryButton.tsyle';
 import Button from '@components/common/Button';
 import { BUTTON_SIZE } from '@components/common/Button/constants';
 import { categoryItem } from '@constants/category';
-import { currentShareList } from '@store/shareList';
+import { currentFilterShareList } from '@store/filterShareList';
 
 const CategoryButton = () => {
-  const setCurrentShareList = useSetRecoilState(currentShareList);
+  const setCurrentFilterShareList = useSetRecoilState(currentFilterShareList);
 
-  const handelClickChangeList = (filter: string) => setCurrentShareList(filter);
+  const handelClickChangeList = (filter: string) => setCurrentFilterShareList(filter);
 
-  const category = categoryItem.map(({ id, name, filter }) => (
-    <Button
-      key={id}
-      size={BUTTON_SIZE.SMALL}
-      onClick={() => {
-        handelClickChangeList(filter);
-      }}
-    >
-      {name}
-    </Button>
-  ));
-
-  return <div>{category}</div>;
+  return (
+    <S.Wrapper>
+      {categoryItem.map(({ id, name, filter }) => (
+        <S.Item key={id}>
+          <Button
+            size={BUTTON_SIZE.SMALL}
+            onClick={() => {
+              handelClickChangeList(filter);
+            }}
+          >
+            {name}
+          </Button>
+        </S.Item>
+      ))}
+    </S.Wrapper>
+  );
 };
 
 export default CategoryButton;
