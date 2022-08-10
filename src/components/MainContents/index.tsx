ccdfd3ce4925e4b5718af3d0bb82cfa93fd74893
JinJeon <nanyong01@gmail.com>
2022-08-10 16:55:45 +0900

    feat : 로그인 여부에 따라 login area 보이도록 설정

--- src/components/MainContents/index.tsx
+++ src/components/MainContents/index.tsx
@@ -1,31 +1,31 @@
 import LoginArea from '@components/LoginArea';
 import * as S from '@components/MainContents/MainContents.style';
 import PreviewShareListLeftImage from '@components/PreviewShareListLeftImage';
+import { shareDeliveryMention, shareIngredientMention } from '@constants/mentions';
+import { SHOW_MORE } from '@constants/words';
 import { listExample } from '@data/shareList';
 
-const shareDeliveryMention = '함께 음식을 나눠먹어요!';
-const shareIngredientMention = '함께 재료를 나눠보아요!';
-const moreMention = '더보기';
 const mainPageShareListCount = 4;
+const isLogin = false;
 
 const MainContents = () => {
   return (
     <S.Wrapper>
       <S.SubTitle>
         {shareDeliveryMention}
-        <S.More>{moreMention}</S.More>
+        <S.More>{SHOW_MORE}</S.More>
       </S.SubTitle>
       <S.PreviewWrapper>
         <PreviewShareListLeftImage data={listExample} count={mainPageShareListCount} />
       </S.PreviewWrapper>
       <S.SubTitle>
         {shareIngredientMention}
-        <S.More>{moreMention}</S.More>
+        <S.More>{SHOW_MORE}</S.More>
       </S.SubTitle>
       <S.PreviewWrapper>
         <PreviewShareListLeftImage data={listExample} count={mainPageShareListCount} />
       </S.PreviewWrapper>
-      <LoginArea />
+      {!isLogin && <LoginArea />}
     </S.Wrapper>
   );
 };
