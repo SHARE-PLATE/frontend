4df38c45343e87c2e6ea9b7cf152975bf818a16c
JinJeon <nanyong01@gmail.com>
2022-08-10 16:09:05 +0900

    feat : main contents 컴포넌트 생성

---
+++ src/components/MainContents/MainContents.style.tsx
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components';
+
+import { subTitle } from '@styles/mixin';
+
+export const Wrapper = styled.div`
+  padding-bottom: 1rem;
+`;
+
+export const SubTitle = styled.div`
+  ${subTitle};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const More = styled.div`
+  ${({ theme: { colors, fonts } }) => css`
+    ${fonts.small};
+    color: ${colors.grey4};
+  `}
+`;
+
+export const PreviewWrapper = styled.div`
+  margin-top: 1rem;
+`;
