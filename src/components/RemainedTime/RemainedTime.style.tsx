ab0182e7ed6ccc61fbd0f59b87318ac224186733
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-08-11 17:21:04 +0900

    feat : 하단에 위치하는 props #47

--- src/components/RemainedTime/RemainedTime.style.tsx
+++ src/components/RemainedTime/RemainedTime.style.tsx
@@ -1,12 +1,32 @@
 import styled, { css } from 'styled-components';
 
-export const RemainedTimeWrapper = styled.div`
+interface LocationBottomStyleType {
+  locationBottom: boolean;
+}
+
+const bottomStyles = css<LocationBottomStyleType>`
+  ${({ locationBottom }) =>
+    locationBottom &&
+    css`
+      bottom: 0.5rem;
+      left: 0.5rem;
+    `}
+
+  ${({ locationBottom }) =>
+    !locationBottom &&
+    css`
+      top: 0.5rem;
+      left: 0.5rem;
+    `}
+`;
+
+export const RemainedTimeWrapper = styled.div<LocationBottomStyleType>`
+  ${bottomStyles}
   ${({ theme: { colors, fonts } }) => css`
     ${fonts.xSmallBold}
     display: inline-block;
     position: absolute;
-    top: 0.5rem;
-    left: 0.5rem;
+
     border-radius: 0.4rem;
     background-color: ${colors.orange2};
     padding: 0.2rem 0.4rem;
