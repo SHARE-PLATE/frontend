566d0a3a9b3b2511af42583dde713e2492f6b958
JinJeon <nanyong01@gmail.com>
2022-08-13 00:34:42 +0900

    style : Category Button 컴포넌트 스타일 수정 #55

--- src/components/CategoryButton/CategoryButton.tsyle.ts
+++ src/components/CategoryButton/CategoryButton.tsyle.ts
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
+export const categoryButtonHeight = '3.6rem';
+
 export const Wrapper = styled.ul`
   display: flex;
+  gap: 0.75rem;
+  height: ${categoryButtonHeight};
+  align-items: center;
 `;
 
 export const Item = styled.li``;
