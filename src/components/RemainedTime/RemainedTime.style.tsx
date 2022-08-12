5dfc512b6d22d263df2849439874db5d5144fd04
JinJeon <nanyong01@gmail.com>
2022-08-12 15:09:03 +0900

    refactor : remained time 컴포넌트 수정에 따른 기타 컴포넌트 수정

--- src/components/RemainedTime/RemainedTime.style.tsx
+++ src/components/RemainedTime/RemainedTime.style.tsx
@@ -11,6 +11,11 @@ export const RemainedTimeWrapper = styled.div<BottomStylesPositionType>`
   ${({ theme: { colors, fonts }, left, right, top, bottom }) => css`
     ${fonts.xSmallBold}
 
+    left: ${left};
+    right: ${right};
+    top: ${top};
+    bottom: ${bottom};
+
     display: inline-block;
     position: absolute;
     top: ${top};
