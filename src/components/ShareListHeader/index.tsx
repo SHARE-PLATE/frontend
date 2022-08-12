4f5a182212dfa3e5607f08fdd26aaec15ffaff5d
JinJeon <nanyong01@gmail.com>
2022-08-13 00:43:23 +0900

    refactor : share list header 컴포넌트의 불필요한 태그 제거 #55

--- src/components/ShareListHeader/index.tsx
+++ src/components/ShareListHeader/index.tsx
@@ -5,14 +5,8 @@ import Icon from '@components/common/Icon';
 const ShareListHeader = () => {
   return (
     <S.Wrapper>
-      <div>
-        <Address />
-      </div>
-      <div>
-        <Icon iconName='NoticeOn' iconSize='LARGE' />
-        {/* 내 주변 아이콘으로 변경 */}
-        <Icon iconName='NoticeOn' iconSize='LARGE' />
-      </div>
+      <Address />
+      <Icon iconName='NoticeOn' iconSize='LARGE' />
     </S.Wrapper>
   );
 };
