6d31f0014050561f21f356cff955128ab8e42113
JinJeon <nanyong01@gmail.com>
2022-08-12 15:28:22 +0900

    refactor : path name 위치 수정에 따른 삭제

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -10,23 +10,11 @@ import ShareForm from '@pages/ShareForm';
 import ShareList from '@pages/ShareList';
 import ProtectedRoute from '@router/ProtectedRoute';
 
-export const pathName = {
-  main: '',
-  shareList: 'share-list',
-  loginCallback: 'login-callback',
-  shareDetail: 'share-detail',
-  notice: 'notice',
-  error: 'error',
-  shareForm: 'share-form',
-  profile: 'profile',
-  other: '*',
-};
-
 export const routes = [
   { path: P.main, element: <Main /> },
   { path: P.loginCallback, element: <LoginCallback /> },
   { path: P.shareList, element: <ShareList /> },
-  { path: P.shareDetail, element: <ShareDetail /> },
+  { path: P.shareDetail + '/:id', element: <ShareDetail /> },
   { path: P.notice, element: <Notice /> },
   { path: P.error, element: <Error /> },
   {
