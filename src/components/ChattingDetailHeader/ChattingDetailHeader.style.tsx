4e9d417134856c4fde72f94cd707093cde342ae8
JinJeon <nanyong01@gmail.com>
2022-08-19 15:11:34 +0900

    feat : chatting detail header 생성 및 적용 #59

---
+++ src/components/ChattingDetailHeader/ChattingDetailHeader.style.tsx
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components';
+
+export const Wrapper = styled.div`
+  ${({ theme: { defaultHeaderHeight } }) => css`
+    height: ${defaultHeaderHeight};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `}
+`;
+
+export const HeaderInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  gap: 0.3rem;
+`;
+
+export const Location = styled.div`
+  ${({ theme: { fonts, colors } }) => css`
+    ${fonts.xSmall};
+    color: ${colors.grey4};
+  `}
+`;
