d2447ace590583bacdec4ff91e922aef116c9c07
JinJeon <nanyong01@gmail.com>
2022-08-21 17:14:39 +0900

    refactor : bottom bar 컴포넌트의 빈 공간 영역 삭제

--- src/components/BottomBar/index.tsx
+++ src/components/BottomBar/index.tsx
@@ -1,4 +1,5 @@
 import * as S from '@components/BottomBar/BottomBar.style';
+import ChattingBar from '@components/ChattingBar';
 import InteractionBar from '@components/InteractionBar';
 import NavigationBar from '@components/NavigationBar';
 import useCheckPathname from '@hooks/useCheckPathname';
@@ -8,17 +9,14 @@ const BottomBar = () => {
 
   const isShareDetail = useCheckPathname({ targetPaths: ['shareDetail'] });
   const isShareForm = useCheckPathname({ targetPaths: ['shareForm'] });
+  const isChattingDetail = useCheckPathname({ targetPaths: ['chattingDetail'] });
 
   if (isShareDetail) bottomBarContent = <InteractionBar />;
   if (isShareForm) bottomBarContent = null;
+  if (isChattingDetail) bottomBarContent = <ChattingBar />;
   if (bottomBarContent === undefined) bottomBarContent = <NavigationBar />;
 
-  return (
-    <>
-      <S.BottomBarArea />
-      <S.BottomBarWrapper>{bottomBarContent}</S.BottomBarWrapper>
-    </>
-  );
+  return <S.BottomBarWrapper>{bottomBarContent}</S.BottomBarWrapper>;
 };
 
 export default BottomBar;
