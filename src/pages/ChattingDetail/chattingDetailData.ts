e4248437aa1f21ece7b9df1f33b23077f328a4ca
JinJeon <nanyong01@gmail.com>
2022-08-21 17:23:24 +0900

    test : chatting detail 페이지에 사용될 임시 데이터 변경

--- src/pages/ChattingDetail/chattingDetailData.ts
+++ src/pages/ChattingDetail/chattingDetailData.ts
@@ -1,34 +1,120 @@
-export const testData = {
-  share: {
-    id: 1,
-    thumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
-    title: '채팅 상세 테스트',
-    price: 10000,
-    originalPrice: 40000,
-    currentRecruitment: 3,
-    finalRecruitment: 4,
+export const testChattingDetailShare = {
+  id: 1,
+  thumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+  title:
+    '채팅 상세 테스트 채팅 상세 테스트 채팅 상세 테스트 채팅 상세 테스트 채팅 상세 테스트 채팅 상세 테스트 채팅 상세 테스트 채팅 상세 테스트 채팅 상세 테스트 채팅 상세 테스트',
+  price: 10000,
+  originalPrice: 40000,
+  currentRecruitment: 3,
+  finalRecruitment: 4,
+};
+
+export const testChattingDetailChats = [
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임1',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:00',
+    writtenByMe: false,
   },
-  chats: [
-    {
-      contents: '안녕하세요',
-      wrtier: '닉네임1',
-      writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
-      writtenDateTime: '2022-12-30 14:00',
-      writtenByMe: 'false',
-    },
-    {
-      contents: '안녕하세요',
-      wrtier: '닉네임2',
-      writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
-      writtenDateTime: '2022-12-30 14:01',
-      writtenByMe: 'true',
-    },
-  ],
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임2',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:01',
+    writtenByMe: true,
+  },
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임1',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:00',
+    writtenByMe: false,
+  },
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임2',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:01',
+    writtenByMe: true,
+  },
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임1',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:00',
+    writtenByMe: false,
+  },
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임2',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:01',
+    writtenByMe: true,
+  },
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임1',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:00',
+    writtenByMe: false,
+  },
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임2',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:01',
+    writtenByMe: true,
+  },
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임1',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:00',
+    writtenByMe: false,
+  },
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임2',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:01',
+    writtenByMe: true,
+  },
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임1',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:00',
+    writtenByMe: false,
+  },
+  {
+    contents:
+      '안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 ',
+    writer: '닉네임2',
+    writerThumbnailImageUrl: 'https://avatars.githubusercontent.com/u/67730358?v=4',
+    writtenDateTime: '2022-12-30 14:01',
+    writtenByMe: true,
+  },
+];
+
+export const testChattingDetailData = {
+  share: testChattingDetailShare,
+  chats: testChattingDetailChats,
 };
 
-export type dataType = typeof testData;
+export type TestChattingDetailShareType = typeof testChattingDetailShare;
 
-export const defaultData = {
-  share: {},
-  chats: [],
-};
+export type TestChattingDetailChatsType = typeof testChattingDetailChats;
+
+export type TestChattingDetailDataType = typeof testChattingDetailData;
