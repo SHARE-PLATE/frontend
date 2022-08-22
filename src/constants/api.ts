06766699ae14e97c36c0a26aff2ecc7eb20870b6
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-08-22 15:55:57 +0900

    refactor : 목데이터 삭제 및 서버 통신 #61

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,4 +2,5 @@ export const API = {
   SHARE_LIST: `/api/shares`,
   SHARE_REGISTRATION: `/api/shares`,
   SHARE_DETAIL: (id?: string) => `/api/shares/${id}`,
+  SHARE_RECOMMENDED: `/api/shares/recommendation`,
 };
