605e0934033acd36f65193fccec88a491d74d8b9
JinJeon <nanyong01@gmail.com>
2022-08-23 19:05:56 +0900

    fix : 주소 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.REACT_APP_BASE_URL,
+      target: 'http://louie-03.com',
       changeOrigin: true,
       pathRewrite: {
         '^/api': '/',
