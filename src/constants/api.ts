cce8aa852e6a98c3967191788c2ed581fb070ea5
JinJeon <nanyong01@gmail.com>
2022-08-29 09:57:59 +0900

    feat : api, words 상수 추가

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -5,7 +5,9 @@ export const API = {
   SHARE_RECOMMENDED: `/api/shares/recommendation`,
   CHATTING_DETAIL: '/api/chatrooms',
   SOCKET: '/api/socket',
+  LOGIN_FORM: '/login/form',
   LOGIN: '/api/login',
   LOGOUT: '/api/logout',
-  LOGIN_FORM: '/login/form',
+  CHECK_LOGIN: '/api/reissue/access-token',
+  WISH_LIST: '/api/wish-list',
 };
