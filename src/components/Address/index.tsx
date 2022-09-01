8aea41e5ca4701e8e6cf2591bc0bc8da1f4bb6e8
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-09-01 15:40:41 +0900

    feat : 주소 포탈 추가  #80

--- src/components/Address/index.tsx
+++ src/components/Address/index.tsx
@@ -1,33 +1,18 @@
-import { useRef } from 'react';
-
-import { useRecoilValue } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 
 import * as S from '@components/Address/Address.style';
-import AddressModal from '@components/ModalContainer/AddressModal';
 import Icon from '@components/common/Icon';
-import useGeolocation from '@hooks/useGeolocation';
-import useModal from '@hooks/useModal';
-import { currentLocation } from '@store/location';
+import { portalState } from '@store/portal';
 
 const Address = () => {
-  const location = useGeolocation();
-  const curLocation = useRecoilValue(currentLocation);
-  const modalRef = useRef<HTMLDivElement>(null);
-  const [isAddressModalOpen, setIsAddressModalOpen] = useModal({ modalRef });
+  const setPortal = useSetRecoilState(portalState);
 
-  const openAddressModal = () => setIsAddressModalOpen(true);
-  const closeAddressModal = () => setIsAddressModalOpen(false);
   return (
-    <S.Wrapper>
-      <S.LocationWrapper onClick={openAddressModal}>
-        {curLocation ? curLocation : '강남역'}
-      </S.LocationWrapper>
+    <S.Wrapper onClick={() => setPortal('address')}>
+      <S.LocationWrapper>강남역</S.LocationWrapper>
       <S.IconWrapper>
         <Icon iconName='ChevronDown' />
       </S.IconWrapper>
-      {isAddressModalOpen && (
-        <AddressModal modalRef={modalRef} closeAddressModal={closeAddressModal} />
-      )}
     </S.Wrapper>
   );
 };
---
+++ src/components/AddressPortal/index.tsx
@@ -0,0 +1,5 @@
+const AddressPortal = () => {
+  return <div></div>;
+};
+
+export default AddressPortal;
