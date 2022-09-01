4cde34d12697c3dc7bd1f92dffd3ac27f874f63a
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-09-01 14:22:39 +0900

    feat : 스타일 코드 분리 #75

--- src/components/PreviewShareList/PreviewShareList.style.ts
+++ src/components/PreviewShareList/PreviewShareList.style.ts
@@ -1,37 +1,46 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const Container = styled.div`
   position: relative;
-  padding-left: 30px;
+  padding-left: 20px;
   width: 100%;
+  height: 100px;
 
   img {
     position: absolute;
     top: 0px;
+    border-radius: 8px;
   }
 `;
 
 export const ListInfo = styled.div`
+  display: flex;
   position: absolute;
-  left: 40%;
+  left: 36%;
+  flex-direction: column;
+  gap: 6px;
   padding: 3px;
+  overflow-wrap: break-word;
 `;
 
 export const Title = styled.div`
-  text-align: left;
+  font-size: 14px;
   font-weight: bold;
+  overflow-wrap: break-word;
 `;
 
 export const Location = styled.div`
-  text-align: left;
   color: #918b8b;
+  font-size: 10px;
 `;
 
 export const Cost = styled.div`
-  text-align: left;
+  font-size: 14px;
   font-weight: bold;
-`;
-
-export const Personnel = styled.div`
-  text-align: left;
 `;
