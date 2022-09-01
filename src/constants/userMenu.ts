17eb2c8d9db9dfc201651e0a38328cd42a1adc60
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-09-01 14:22:59 +0900

    feat : 키워드 부분 상수 추가 #75

--- src/constants/userMenu.ts
+++ src/constants/userMenu.ts
@@ -8,4 +8,5 @@ export const userMenu: userMenuType[] = [
   { id: 1, title: '공지사항', link: '이동' },
   { id: 2, title: '자주 묻는 질문', link: '이동' },
   { id: 3, title: '고객센터', link: '이동' },
+  { id: 4, title: '키워드 알림', link: './keyword' },
 ];
