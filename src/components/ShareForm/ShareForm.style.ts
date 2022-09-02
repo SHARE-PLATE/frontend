c68a47d68dd70a9a6f3a72902ec6882b74a8062f
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-09-02 16:49:44 +0900

    feat : 모집 스타일 변경 #81

--- src/components/ShareForm/ShareForm.style.ts
+++ src/components/ShareForm/ShareForm.style.ts
@@ -2,14 +2,15 @@ import styled from 'styled-components';
 
 import { flexCenter } from '@styles/mixin';
 
-export const FileContainer = styled.div`
+//FileContainer.tsx
+export const FileWrapper = styled.div`
   display: flex;
   margin-top: 15px;
 `;
 
 export const FileLabel = styled.label`
   border-radius: 4px;
-  background-color: #efefef;
+  background-color: ${({ theme }) => theme.colors.grey1};
   cursor: pointer;
   padding: 6px 25px;
   text-align: center;
@@ -20,6 +21,7 @@ export const FileForm = styled.input`
   display: none;
 `;
 
+//TextContainer.tsx
 export const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -29,7 +31,7 @@ export const TextContainer = styled.div`
 export const TowTextBlock = styled.div`
   display: flex;
   gap: 12px;
-  justify-content: space-between;
+  justify-content: flex-start;
 `;
 
 export const LongTextBlock = styled.div`
@@ -43,11 +45,33 @@ export const DateInputForm = styled.input`
 
   padding: 11px 16px;
   width: 100%;
-  height: 40px;
+  height: 41px;
   line-height: 20px;
   letter-spacing: 1px;
 `;
+export const CountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: calc(50% - 6px);
+  gap: 10px;
+`;
+export const ButtonContainer = styled.div`
+  ${flexCenter}
 
+  border: 1px solid ${({ theme }) => theme.colors.grey3};
+  border-radius: 4px;
+`;
+
+export const CountButton = styled.button`
+  padding: 3px 10px;
+  margin-top: 5px;
+`;
+
+export const OptionContainer = styled.div`
+  height: 41px;
+`;
+
+//ContentDescription.tsx
 export const DescriptionWrapper = styled.div`
   min-height: 236px;
 `;
--- src/components/common/InputForm/InputForm.style.ts
+++ src/components/common/InputForm/InputForm.style.ts
@@ -11,7 +11,7 @@ export const InputBox = styled.input`
 
   padding: 11px 16px;
   width: 100%;
-  height: 40px;
+  height: 41px;
   line-height: 20px;
 
   letter-spacing: 1px;
