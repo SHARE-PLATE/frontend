bda55782e5ec082fc1e2ed3da670e2c9c690ea39
JinJeon <nanyong01@gmail.com>
2022-09-02 10:54:41 +0900

    refactor : modal 컴포넌트 재사용성을 위한 리펙토링

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,19 +1,15 @@
 import * as S from '@components/common/Modal/Modal.style';
 
 export interface ModalPropsTypes {
-  position: string;
+  position: S.PositionType;
   closeModal?: () => void;
   children?: JSX.Element;
+  isFull?: boolean;
 }
 
-export const MODAL_POSITION = {
-  RIGHT: 'Right',
-  CENTER: 'Center',
-};
-
-const Modal = ({ position, closeModal, children }: ModalPropsTypes) => {
+const Modal = ({ position, closeModal, children, isFull }: ModalPropsTypes) => {
   return (
-    <S.ModalContainer position={position}>
+    <S.ModalContainer position={position} isFull={isFull}>
       {closeModal && <S.CloseButton onClick={closeModal}>닫기</S.CloseButton>}
       {children}
     </S.ModalContainer>
