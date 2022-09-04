20ed1b337b58ccbf1614e0d421ff2e356081f701
JinJeon <nanyong01@gmail.com>
2022-09-05 01:14:23 +0900

    refactor : address detail, setting, list 분리에 따른 address portal 리펙토링 #80

--- src/components/AddressPortal/AddressPortal.style.tsx
+++ src/components/AddressPortal/AddressPortal.style.tsx
@@ -19,11 +19,7 @@ export const Wrapper = styled.div`
   `}
 `;
 
-export const TopWrapper = styled.div`
-  ${({ theme: { defaultPadding } }) => css`
-    ${defaultPadding}
-  `}
-`;
+export const TopWrapper = styled.div``;
 
 export const Header = styled.div`
   ${({ theme: { defaultHeaderHeight } }) => css`
@@ -35,8 +31,8 @@ export const Header = styled.div`
   `}
 `;
 
-export const CloseBtn = styled.button<{ isList: boolean }>`
-  ${({ theme: { colors }, isList }) => css`
+export const HeaderBtn = styled.button<{ isSearching: boolean }>`
+  ${({ theme: { colors }, isSearching }) => css`
     z-index: 2;
     left: 0;
     display: flex;
@@ -46,7 +42,7 @@ export const CloseBtn = styled.button<{ isList: boolean }>`
     max-width: 50px;
     pointer-events: none;
 
-    ${!isList &&
+    ${!isSearching &&
     css`
       pointer-events: all;
       svg {
@@ -72,19 +68,19 @@ export const HeaderTitle = styled.div`
 export const AddressInputArea = styled.form`
   ${({ theme: { colors } }) => css`
     display: flex;
+    height: 3.125rem;
     align-items: center;
     padding: 0.8rem;
     background-color: ${colors.grey1};
     width: 100%;
-    gap: 0.8rem;
-    height: 3.125rem;
+    gap: 0.5rem;
     border-radius: 0.25rem;
   `}
 `;
 
 export const AddressInput = styled.input`
   ${({ theme: { colors, fonts } }) => css`
-    ${fonts.mediumRegular};
+    ${fonts.largeRegular};
 
     background-color: ${colors.grey1};
     border: none;
@@ -110,121 +106,5 @@ export const AroundSearchButton = styled.button`
     height: 3.37rem;
     z-index: 5;
     gap: 0.58rem;
-  `}
-`;
-
-export const HomeAndCompanyWrapper = styled.div`
-  ${({ theme: { defaultPadding, fonts } }) => css`
-    ${fonts.medium};
-    ${defaultPadding};
-
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    display: flex;
-    flex-direction: column;
-    gap: 1.25rem;
-
-    > button {
-      display: flex;
-      gap: 0.75rem;
-      align-items: center;
-    }
-  `}
-`;
-
-export const RegisteredAddressList = styled.div``;
-
-export const RegisteredAddress = styled.div`
-  display: flex;
-  gap: 0.75rem;
-  padding: 1rem 0;
-  justify-content: space-between;
-  align-items: flex-start;
-
-  > :first-child,
-  > :last-child {
-    height: 1rem;
-  }
-`;
-
-export const RegisteredAddressText = styled.div`
-  ${({ theme: { fonts, colors } }) => css`
-    display: flex;
-    flex-direction: column;
-    text-align: left;
-    flex-grow: 1;
-    gap: 0.25rem;
-
-    > :nth-child(1) {
-      ${fonts.mediumRegular}
-      color: ${colors.grey6};
-      line-height: 1rem;
-    }
-    > :nth-child(2) {
-      ${fonts.smallLight};
-      color: ${colors.grey4};
-      line-height: 1rem;
-    }
-  `}
-`;
-
-export const AddressListWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  overflow: scroll;
-`;
-
-export const AddressListItem = styled.div`
-  ${({ theme: { colors, fonts } }) => css`
-    ${fonts.mediumRegular};
-
-    padding: 1rem 0;
-    border-bottom: 1px solid ${colors.grey1};
-
-    :last-child {
-      border-bottom: none;
-    }
-  `}
-`;
-
-export const AddressListRoadName = styled.div`
-  ${({ theme: { fonts, colors } }) => css`
-    display: flex;
-    gap: 0.25rem;
-    align-items: center;
-    color: ${colors.grey4};
-    margin-top: 0.4rem;
-
-    ${fonts.smallLight};
-
-    > :nth-child(1) {
-      ${fonts.xSmallLight};
-      color: ${colors.blue0};
-      border: 1px solid ${colors.grey2};
-      border-radius: 0.1rem;
-      padding: 0.15rem 0.2rem;
-    }
-  `}
-`;
-
-export const noAddressList = styled.div`
-  ${({ theme: { colors, fonts } }) => css`
-    ${fonts.medium};
-
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1.5rem;
-    text-align: center;
-    justify-content: center;
-    max-height: 30rem;
-    white-space: pre;
-    line-height: 1.2rem;
-    color: ${colors.grey7};
-
-    path {
-      stroke: ${colors.grey4};
-    }
   `}
 `;
--- src/components/AddressPortal/index.tsx
+++ src/components/AddressPortal/index.tsx
@@ -1,137 +1,70 @@
-import { ChangeEvent, FormEvent, ReactElement, useRef, useState } from 'react';
+import { ChangeEvent, useRef, useState } from 'react';
 
-import { getAddressWithKeyword } from '@api/address';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+
+import AddressDetail from '@components/AddressDetail';
+import AddressList from '@components/AddressList';
 import * as S from '@components/AddressPortal/AddressPortal.style';
-import AddressSearchTip from '@components/AddressSearchTip';
+import AddressSetting from '@components/AddressSetting';
 import Portal from '@components/Portal';
 import Icon from '@components/common/Icon';
 import { searchAroundMention, searchWayMention } from '@constants/mentions';
-import { ADD_COMPANY, ADD_HOME, JIBUN, ROAD_NAME, SET_ADDRESS } from '@constants/words';
-import { useDebounce } from '@hooks/useDebounce';
-
-const 임시주소 = '하나로마트';
-const 임시지번 = '충북 청주시 예체로1';
-const 임시주소들 = [{ id: 0, address: 임시주소, jibun: 임시지번 }];
-
-const addressSearchingDelay = 500; // ms
+import { SEARCH_ADDRESS, SET_ADDRESS } from '@constants/words';
+import { SelectedAddress } from '@store/address';
+import { portalState } from '@store/portal';
 
 const AddressPortal = () => {
   const closeBtn = useRef<HTMLButtonElement>(null);
   const [addressValue, setAddressValue] = useState('');
-  const [addressList, setAddressList] = useState<ReactElement>();
-  const [isList, setIsList] = useState(false);
-
-  const registeredAddressList = 임시주소들.map(({ id, address, jibun }) => (
-    <S.RegisteredAddress key={id}>
-      <Icon iconName='LocationMarker' />
-      <S.RegisteredAddressText>
-        <div>{address}</div>
-        <div>{`[${JIBUN}]${jibun}`}</div>
-      </S.RegisteredAddressText>
-      <Icon iconName='DeleteCircle' />
-    </S.RegisteredAddress>
-  ));
-
-  const setAddressListWithData = async (value: string) => {
-    if (!value) return;
-
-    const addressListData = await getAddressWithKeyword(value);
-    if (!addressListData.length) {
-      const noAddressList = (
-        <S.noAddressList>
-          <Icon iconName='Search' iconSize={4.8} />
-          {'검색 결과가 없습니다.\n지번, 도로명, 건물명을 입력해주세요.'}
-        </S.noAddressList>
-      );
-      setAddressList(noAddressList);
-      return;
-    }
-
-    const newAddressList = addressListData.map(({ id, place_name, road_address_name }) => (
-      <S.AddressListItem key={id}>
-        {place_name}
-        <S.AddressListRoadName>
-          <div>{ROAD_NAME}</div>
-          <div>{road_address_name}</div>
-        </S.AddressListRoadName>
-      </S.AddressListItem>
-    ));
-
-    setAddressList(<>{newAddressList}</>);
-  };
+  const [isSearching, setIsSearching] = useState(false);
+  const setPortal = useSetRecoilState(portalState);
+  const selectedAddress = useRecoilValue(SelectedAddress);
+  const isSelectedAddress = !!selectedAddress.x;
 
   const handleChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
     const newAddressValue = event.target.value;
     setAddressValue(newAddressValue);
   };
 
-  const handleSubmitAddress = async (event: FormEvent) => {
-    event.preventDefault();
-    setAddressListWithData(addressValue);
-  };
-
-  useDebounce({
-    func: () => setAddressListWithData(addressValue),
-    delay: addressSearchingDelay,
-    deps: [addressValue],
-  });
-
   return (
     <Portal type='full' portalName='address' closeBtn={closeBtn}>
       <S.Wrapper>
-        <S.TopWrapper>
-          <S.Header
-            onClick={() => {
-              setIsList(false);
-            }}
-          >
-            <S.CloseBtn ref={closeBtn} isList={isList}>
-              <Icon iconName={!isList ? 'X_Icon' : 'Back'} />
-            </S.CloseBtn>
-            <S.HeaderTitle>{SET_ADDRESS}</S.HeaderTitle>
-          </S.Header>
-
-          <S.AddressInputArea onSubmit={handleSubmitAddress}>
-            <button>
-              <Icon iconName='SearchSmall' iconSize={1.125} />
-            </button>
-            <S.AddressInput
-              value={addressValue}
-              onFocus={() => setIsList(true)}
-              onChange={handleChangeInput}
-              placeholder={searchWayMention}
-              maxLength={45}
-            />
-          </S.AddressInputArea>
-
-          <S.AroundSearchButton>
-            <Icon iconName='LocationMarker' />
-            {searchAroundMention}
-          </S.AroundSearchButton>
-        </S.TopWrapper>
-
-        {!isList && (
+        {!isSelectedAddress && (
           <>
-            <S.HomeAndCompanyWrapper>
-              <button>
-                <Icon iconName='HomeAdd' />
-                {ADD_HOME}
-              </button>
-              <button>
-                <Icon iconName='BriefCase' />
-                {ADD_COMPANY}
-              </button>
-            </S.HomeAndCompanyWrapper>
+            <S.TopWrapper>
+              <S.Header onClick={() => setIsSearching(false)}>
+                <S.HeaderBtn
+                  ref={closeBtn}
+                  isSearching={isSearching}
+                  onClick={() => !isSearching && setPortal(null)}
+                >
+                  <Icon iconName={!isSearching ? 'X_Icon' : 'Back'} />
+                </S.HeaderBtn>
+                <S.HeaderTitle>{!isSearching ? SET_ADDRESS : SEARCH_ADDRESS}</S.HeaderTitle>
+              </S.Header>
 
-            <S.RegisteredAddressList>{registeredAddressList}</S.RegisteredAddressList>
+              <S.AddressInputArea onSubmit={(event) => event.preventDefault()}>
+                <button>
+                  <Icon iconName='SearchSmall' iconSize={1.125} />
+                </button>
+                <S.AddressInput
+                  value={addressValue}
+                  onFocus={() => setIsSearching(true)}
+                  onChange={handleChangeInput}
+                  placeholder={searchWayMention}
+                  maxLength={45}
+                />
+              </S.AddressInputArea>
+
+              <S.AroundSearchButton>
+                <Icon iconName='LocationMarker' />
+                {searchAroundMention}
+              </S.AroundSearchButton>
+            </S.TopWrapper>
+            {!isSearching ? <AddressSetting /> : <AddressList addressValue={addressValue} />}
           </>
         )}
-
-        {isList && !addressValue.length && <AddressSearchTip />}
-
-        {isList && !!addressValue.length && (
-          <S.AddressListWrapper>{addressList}</S.AddressListWrapper>
-        )}
+        {isSelectedAddress && <AddressDetail setIsSearching={setIsSearching} />}
       </S.Wrapper>
     </Portal>
   );
