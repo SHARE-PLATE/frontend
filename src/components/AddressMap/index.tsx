8f38bfd6f9ed3a9a1b22adab43b5f810f87ee314
JinJeon <nanyong01@gmail.com>
2022-09-07 17:29:30 +0900

    refactor : address map의 맵 보여주기 기능 활성화를 위한 리펙토링 #80

--- src/components/AddressMap/index.tsx
+++ src/components/AddressMap/index.tsx
@@ -1,33 +1,34 @@
-/*global kakao */
-
 import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 
 import * as S from '@components/AddressMap/AddressMap.style';
 import Icon from '@components/common/Icon';
+import { SET_ADDRESS } from '@constants/words';
 import { SelectedAddressType } from '@store/selectedAddress';
 
 import { locationMarker } from './locationMarker';
 
 interface AddressMapPropsType extends SelectedAddressType {
   setIsMap: Dispatch<SetStateAction<boolean>>;
+  handleClickFinishBtn: () => void;
 }
 
 const { kakao } = window as any;
 
 const AddressMap = ({
-  x: lat,
-  y: lng,
-  address_name,
+  x,
+  y,
+  place_name,
   road_address_name,
   setIsMap,
+  handleClickFinishBtn,
 }: AddressMapPropsType) => {
   const [mapState, setMapState] = useState(null);
   const mapRef = useRef(null);
+  const position = new kakao.maps.LatLng(y, x);
 
   const handleClickBackBtn = () => setIsMap(false);
 
   const initMap = () => {
-    const position = new kakao.maps.LatLng(lat, lng);
     const options = { center: position, level: 3 };
     const newMap = new kakao.maps.Map(mapRef.current, options);
 
@@ -35,7 +36,6 @@ const AddressMap = ({
   };
 
   const drawOverlay = () => {
-    const position = new kakao.maps.LatLng(lat, lng);
     const overlay = new kakao.maps.CustomOverlay({ position, content: locationMarker });
 
     overlay.setMap(mapState);
@@ -58,8 +58,9 @@ const AddressMap = ({
         <S.Map ref={mapRef} />
       </S.MapContainer>
       <S.LocationDescription>
+        <div>{place_name}</div>
         <div>{road_address_name}</div>
-        <div>{address_name}</div>
+        <S.FinishBtn onClick={handleClickFinishBtn}>{SET_ADDRESS}</S.FinishBtn>
       </S.LocationDescription>
     </S.Wrapper>
   );
