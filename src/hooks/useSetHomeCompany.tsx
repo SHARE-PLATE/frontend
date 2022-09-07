75b40a0a8f17503ef4a13ba0e7d2ae7f81f795ca
JinJeon <nanyong01@gmail.com>
2022-09-07 17:30:08 +0900

    refactor : 기타 리펙토링

--- src/hooks/useSetHomeCompany.tsx
+++ src/hooks/useSetHomeCompany.tsx
@@ -1,31 +1,31 @@
 import { useRecoilState } from 'recoil';
 import { v4 as getRandomKey } from 'uuid';
 
-import { COMPANY, HOME } from '@constants/words';
+import { ADDRESS_RECENT, COMPANY, HOME } from '@constants/words';
 import { addressRecentState } from '@store/localStorage';
 import { setLocalStorageInfo } from '@utils/localStorage';
 
 type UseSetHomeCompanyParams = {
   target: typeof HOME | typeof COMPANY;
-  lat: string;
-  lng: string;
+  lat: string | number;
+  lng: string | number;
   id?: string;
-  road_address_name: string;
+  place_name: string;
 };
 
 export const useSetHomeCompany = () => {
   const [addressRecent, setAddressRecent] = useRecoilState(addressRecentState); // 수정 필요
 
-  return ({ target, lat, lng, road_address_name, id }: UseSetHomeCompanyParams) => {
+  return ({ target, lat, lng, place_name, id }: UseSetHomeCompanyParams) => {
     const newAddressRecent = new Map(addressRecent);
     newAddressRecent.set(target, {
       lat,
       lng,
       id: id || getRandomKey(),
-      road_address_name,
+      place_name,
     });
 
     setAddressRecent(newAddressRecent);
-    setLocalStorageInfo({ key: 'addressRecent', info: [...newAddressRecent] });
+    setLocalStorageInfo({ key: ADDRESS_RECENT, info: [...newAddressRecent] });
   };
 };
