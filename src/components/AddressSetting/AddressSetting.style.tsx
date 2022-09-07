57495540ad7a6af4917f4b3bd9b0953c450657d9
JinJeon <nanyong01@gmail.com>
2022-09-07 17:24:03 +0900

    refactor : 새로 생성한 컴포넌트로 address setting 컴포넌트 리펙토링 #80

--- src/components/AddressSetting/AddressSetting.style.tsx
+++ src/components/AddressSetting/AddressSetting.style.tsx
@@ -22,47 +22,13 @@ export const HomeCompanyText = styled.div`
   ${({ theme: { fonts, colors } }) => css`
     ${fonts.medium};
     text-align: left;
-
-    > :nth-child(2) {
-      color: ${colors.grey4};
-      ${fonts.smallLight};
-    }
-  `}
-`;
-
-export const RegisteredAddressList = styled.div``;
-
-export const RegisteredAddress = styled.div`
-  cursor: pointer;
-  display: flex;
-  gap: 0.75rem;
-  padding: 1rem 0;
-  justify-content: space-between;
-  align-items: flex-start;
-
-  > :first-child,
-  > :last-child {
-    height: 1rem;
-  }
-`;
-
-export const RegisteredAddressText = styled.div`
-  ${({ theme: { fonts, colors } }) => css`
-    display: flex;
-    flex-direction: column;
-    text-align: left;
     flex-grow: 1;
-    gap: 0.25rem;
 
-    > :nth-child(1) {
-      ${fonts.mediumRegular}
-      color: ${colors.grey6};
-      line-height: 1rem;
-    }
     > :nth-child(2) {
-      ${fonts.smallLight};
       color: ${colors.grey4};
-      line-height: 1rem;
+      ${fonts.smallLight};
     }
   `}
 `;
+
+export const AddressRegisteredList = styled.div``;
--- src/components/AddressSetting/index.tsx
+++ src/components/AddressSetting/index.tsx
@@ -1,77 +1,26 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
+import { v4 as getRandomKey } from 'uuid';
 
+import AddressHomeCompany from '@components/AddressHomeCompany';
+import addressHomeCompanyInfo from '@components/AddressHomeCompany/addressHomeCompanyInfo';
+import AddressRegistered from '@components/AddressRegistered';
 import * as S from '@components/AddressSetting/AddressSetting.style';
-import Icon from '@components/common/Icon';
-import {
-  ADD_COMPANY,
-  ADD_HOME,
-  COMPANY,
-  COMPANY_KOR,
-  HOME,
-  HOME_KOR,
-  JIBUN,
-} from '@constants/words';
-import { useSetHomeCompany } from '@hooks/useSetHomeCompany';
 import { addressRecentState } from '@store/localStorage';
-import { currentLatitudeLongitude, currentLocation } from '@store/location';
 
 const AddressSetting = () => {
-  const [{ lat: curLat, lng: curLng }, setCurLatLng] = useRecoilState(currentLatitudeLongitude);
-  const [curLocation, setCurLocation] = useRecoilState(currentLocation);
   const addressRecent = useRecoilValue(addressRecentState); // 수정 필요
-  const setHomeCompany = useSetHomeCompany();
   const addressRecentList = [...addressRecent.values()];
-  const registeredAddressList = addressRecentList.map(
-    ({ id, road_address_name, address_name, place_name, lat, lng }) => {
-      if (!address_name) return;
-
-      const isCurLocation = lat === curLat && lng === curLng;
-      const handleClick = () => {
-        if (!road_address_name) return;
-
-        setCurLatLng({ lat, lng });
-        setCurLocation(road_address_name);
-      };
-
-      return (
-        <S.RegisteredAddress key={id} onClick={handleClick}>
-          <Icon iconName='LocationMarker' />
-          <S.RegisteredAddressText>
-            <div>{road_address_name}</div>
-            <div>{`[${JIBUN}] ${address_name} ${place_name}`}</div>
-          </S.RegisteredAddressText>
-          <Icon iconName={!isCurLocation ? 'DeleteCircle' : 'CheckCircleColor'} />
-        </S.RegisteredAddress>
-      );
-    },
-  );
-  const registeredHome = addressRecent.get(HOME);
-  const registeredCompany = addressRecent.get(COMPANY);
-
-  const handleClickHomeCompany = (target: typeof HOME | typeof COMPANY) => {
-    setHomeCompany({ target, lat: curLat, lng: curLng, road_address_name: curLocation });
-  };
+  const addressRegisteredList = addressRecentList.map((addressInfo) => (
+    <AddressRegistered key={addressInfo.id} {...addressInfo} />
+  ));
+  const AddressHomeCompanyBtns = addressHomeCompanyInfo.map((info) => (
+    <AddressHomeCompany key={getRandomKey()} {...info} />
+  ));
 
   return (
     <>
-      <S.HomeCompanyWrapper>
-        <S.HomeCompanyBtn onClick={() => handleClickHomeCompany('HOME')}>
-          <Icon iconName='HomeAdd' />
-          <S.HomeCompanyText>
-            <div>{registeredHome ? HOME_KOR : ADD_HOME}</div>
-            {registeredHome && <div>{registeredHome.road_address_name}</div>}
-          </S.HomeCompanyText>
-        </S.HomeCompanyBtn>
-        <S.HomeCompanyBtn onClick={() => handleClickHomeCompany('COMPANY')}>
-          <Icon iconName='BriefCase' />
-          <S.HomeCompanyText>
-            <div>{registeredCompany ? COMPANY_KOR : ADD_COMPANY}</div>
-            {registeredCompany && <div>{registeredCompany.road_address_name}</div>}
-          </S.HomeCompanyText>
-        </S.HomeCompanyBtn>
-      </S.HomeCompanyWrapper>
-
-      <S.RegisteredAddressList>{registeredAddressList}</S.RegisteredAddressList>
+      <S.HomeCompanyWrapper>{AddressHomeCompanyBtns}</S.HomeCompanyWrapper>
+      <S.AddressRegisteredList>{addressRegisteredList}</S.AddressRegisteredList>
     </>
   );
 };
