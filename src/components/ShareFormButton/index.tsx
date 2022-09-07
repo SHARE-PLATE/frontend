c8d59bd9cbb80783633d070895f3a4d5f4e11839
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-09-07 17:06:45 +0900

    feat : 쉐어 폼 모달 추가 #83

--- src/components/ShareFormButton/index.tsx
+++ src/components/ShareFormButton/index.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
 
 import * as S from '@components/ShareFormButton/ShareFormButton.style';
 import Icon from '@components/common/Icon';
+import Modal from '@components/common/Modal';
 import useCheckPathname from '@hooks/useCheckPathname';
+import useModal from '@hooks/useModal';
 
 const ShareFormButton = () => {
   const navigate = useNavigate();
-  const [isOpen, setIsOpen] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+  const [isModalOpen, setIsModalOpen] = useModal({ modalRef });
   const isShareFormOrProfile = useCheckPathname({ targetPaths: ['shareForm', 'profile'] });
-  const showMenuBar = () => setIsOpen(!isOpen);
+  const showMenuBar = () => setIsModalOpen(!isModalOpen);
 
   return (
-    <S.Wrapper onClick={showMenuBar} isShareFormOrProfile={isShareFormOrProfile}>
-      {isOpen ? (
-        <>
-          <span>토글</span>
-          <S.DeliveryButton onClick={() => navigate('/share-registration/delivery')}>
-            <span>배달쉐어</span>
-          </S.DeliveryButton>
-          <S.IngredientButton onClick={() => navigate('/share-registration/ingredient')}>
-            <span>재료쉐어</span>
-          </S.IngredientButton>
-        </>
-      ) : (
-        <button>
-          <Icon iconName='Form' />
-        </button>
-      )}
-    </S.Wrapper>
+    <Modal isFull={isModalOpen} type='underRight'>
+      <S.Wrapper onClick={showMenuBar} isShareFormOrProfile={isShareFormOrProfile}>
+        {isModalOpen ? (
+          <>
+            <button>
+              <Icon iconName='FormClose' iconSize={3.5} />
+            </button>
+            <S.ButtonContainer>
+              <S.DeliveryButton onClick={() => navigate('/share-registration/delivery')}>
+                <Icon iconName='AddDelivery' iconSize='LARGE' />
+                <span>배달쉐어</span>
+              </S.DeliveryButton>
+              <S.IngredientButton onClick={() => navigate('/share-registration/ingredient')}>
+                <Icon iconName='AddIngredient' iconSize='LARGE' />
+                <span>재료쉐어</span>
+              </S.IngredientButton>
+            </S.ButtonContainer>
+          </>
+        ) : (
+          <button>
+            <Icon iconName='Form' iconSize={4.5} />
+          </button>
+        )}
+      </S.Wrapper>
+    </Modal>
   );
 };
 
