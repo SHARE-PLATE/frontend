a85aae9b59345fdca14b772f6232e958ba8fe180
JinJeon <nanyong01@gmail.com>
2022-09-07 17:18:57 +0900

    feat : constants 추가

--- src/constants/words.ts
+++ src/constants/words.ts
@@ -22,6 +22,7 @@ export const RECENT_KEYWORD = '최근 검색어';
 export const LOGIN_SIGNUP = '로그인 / 회원가입';
 export const SHOW_MORE = '더보기';
 export const CHATTING = '채팅하기';
+export const FINISH = '완료';
 export const PARTICIPATING = '참여하기';
 export const ADD_HOME = '집 추가';
 export const ADD_COMPANY = '회사 추가';
