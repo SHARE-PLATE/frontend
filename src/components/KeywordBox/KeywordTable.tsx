4f37db0be74c64b841f228be412fb391074c9aab
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-09-15 12:49:58 +0900

    refactor : props 변경 및 타입 변경 #86

--- src/components/KeywordBox/KeywordTable.tsx
+++ src/components/KeywordBox/KeywordTable.tsx
@@ -1,37 +1,15 @@
-import { useNavigate } from 'react-router-dom';
-
 import * as S from '@components/KeywordBox/KeywordBox.style';
-import Icon from '@components/common/Icon';
-import { addKeyword } from '@constants/mentions';
+import { keywordsType } from '@type/keyword';
 
-interface KeywordTablePropsType {
-  location: string;
-  keyword: string[];
-}
-
-const KeywordTable = ({ location, keyword }: KeywordTablePropsType) => {
-  const navigator = useNavigate();
-
+const KeywordTable = ({ keywords }: { keywords: keywordsType[] }) => {
   return (
     <S.Wrapper>
-      {keyword.length ? (
-        <>
-          <S.SubTitle>알림 키워드</S.SubTitle>
-          <S.Item>
-            {keyword.map((item: string, idx: number) => (
-              <S.ItemText key={idx}>{item}</S.ItemText>
-            ))}
-          </S.Item>
-        </>
-      ) : (
-        <S.FailedContent>
-          <Icon
-            iconName='Plus'
-            handleClick={() => navigator('./add-keyword', { state: location })}
-          />
-          <S.FailedText>{addKeyword}</S.FailedText>
-        </S.FailedContent>
-      )}
+      <S.SubTitle>알림 키워드</S.SubTitle>
+      <S.Item>
+        {keywords.map(({ id, contents }: keywordsType) => (
+          <S.ItemText key={id}>{contents}</S.ItemText>
+        ))}
+      </S.Item>
     </S.Wrapper>
   );
 };
