c1572de7ff06a9a0f9d301cb1234571895ec9ea5
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-09-15 12:39:20 +0900

    refactor : 서버 데이터 요청 #86

--- src/components/RegisteredKeyword/index.tsx
+++ src/components/RegisteredKeyword/index.tsx
@@ -1,8 +1,20 @@
+import { useRecoilValue } from 'recoil';
+
 import * as S from '@components/RegisteredKeyword/RegisteredKeyword.style';
 import Icon from '@components/common/Icon';
 import { keyword } from '@data/keyword';
-const RegisteredKeyword = () => {
+import { getRegisteredKeywordData } from '@store/keyword';
+interface RegisteredKeywordPropsType {
+  regionName: string;
+}
+
+const RegisteredKeyword = ({ regionName }: RegisteredKeywordPropsType) => {
   const exampleKeyword = keyword;
+
+  const { state, contents } = useRecoilValue(getRegisteredKeywordData(regionName)); //에러
+
+  console.log(state);
+  console.log(contents);
 
   return (
     <div>
@@ -19,6 +31,29 @@ const RegisteredKeyword = () => {
       </S.ContentContainer>
     </div>
   );
+
+  // switch (state) {
+  //   case 'hasValue':
+  //     return (
+  //       <div>
+  //         <S.SubHeader>
+  //           등록된 키워드 {exampleKeyword.length} <S.MaxKeyword>/ 10</S.MaxKeyword>
+  //         </S.SubHeader>
+  //         <S.ContentContainer>
+  //           {exampleKeyword.map((item) => (
+  //             <S.ContentBox key={item}>
+  //               <S.ContentItem>{item}</S.ContentItem>
+  //               <Icon iconName='X_Icon' />
+  //             </S.ContentBox>
+  //           ))}
+  //         </S.ContentContainer>
+  //       </div>
+  //     );
+  //   case 'loading':
+  //     return <div>로딩 페이지</div>;
+  //   case 'hasError':
+  //     return <div>에러 페이지</div>;
+  // }
 };
 
 export default RegisteredKeyword;
