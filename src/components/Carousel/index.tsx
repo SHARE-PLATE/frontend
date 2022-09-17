e2fcb67c977ca34627e66cae5b689ad55ab8fcd4
JinJeon <nanyong01@gmail.com>
2022-09-17 20:56:15 +0900

    refactor : carousel 스타일 조건 추가 #93

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -7,21 +7,30 @@ import * as S from '@components/Carousel/Carousel.style';
 interface CarouselPropsType {
   settings: Settings;
   contents: ReactElement[];
+  isCount?: boolean;
   width?: string;
   height?: string;
 }
 
-const Carousel = ({ settings, contents, width = '100%', height = '6rem' }: CarouselPropsType) => {
+const Carousel = ({
+  settings,
+  contents,
+  isCount = true,
+  width = '100%',
+  height = '6rem',
+}: CarouselPropsType) => {
   const [current, setCurrent] = useState(0);
 
   const showCurIndex = (curIndex: number) => setCurrent(curIndex);
 
   return (
     <S.CarouselWrapper width={width} height={height}>
-      <S.CarouselCount>
-        <S.ShowedIndexWrapper>{current + 1}</S.ShowedIndexWrapper>
-        {` / ${contents.length}`}
-      </S.CarouselCount>
+      {isCount && (
+        <S.CarouselCount>
+          <S.ShowedIndexWrapper>{current + 1}</S.ShowedIndexWrapper>
+          {` / ${contents.length}`}
+        </S.CarouselCount>
+      )}
       <S.CarouselSlider {...settings} afterChange={showCurIndex}>
         {contents}
       </S.CarouselSlider>
