d86691c340ff0d6cea6da2cdc282f1e39bbe9d63
JinJeon <nanyong01@gmail.com>
2022-09-17 12:47:35 +0900

    refactor : preview share list center image를 위한 컴포넌트 리펙토링 #93

--- src/components/PreviewShareListCenterImage/PreviewShareListCenterImage.style.tsx
+++ src/components/PreviewShareListCenterImage/PreviewShareListCenterImage.style.tsx
@@ -8,20 +8,26 @@ export const Wrapper = styled.div`
   display: flex;
   gap: 0.75rem;
   padding: 1rem 0;
-  width: 100%;
+  width: calc(100% + 1rem);
   overflow-y: scroll;
+
+  > :last-child {
+    margin-right: 1rem;
+  }
 `;
 
 export const Container = styled.div`
   ${({ theme: { fonts } }) => css`
     ${fonts.medium}
     display: flex;
+    border-radius: 0.5rem;
     flex-direction: column;
-
-    img {
-      border-radius: 20px;
-    }
+    box-shadow: 4px 5px 5px #00000011;
   `}
+`;
+
+export const ImgWrapper = styled.div`
+  position: relative;
 `;
 
 export const NoListContainer = styled.div`
@@ -34,11 +40,8 @@ export const NoListContainer = styled.div`
     text-align: center;
     line-height: 1.2rem;
     white-space: pre;
+    width: calc(100% - 1rem);
     color: ${colors.grey7};
-
-    img {
-      border-radius: 20px;
-    }
   `}
 `;
 
@@ -46,6 +49,7 @@ export const ShareInfo = styled.div`
   ${({ theme: { fonts } }) => css`
     ${fonts.medium}
 
+    padding: 0.5rem 0.4rem;
     width: 110px;
     overflow: hidden;
     text-align: left;
