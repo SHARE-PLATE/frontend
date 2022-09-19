4f2fbc30d3a693814242b46c29f4d565488c932c
JinJeon <nanyong01@gmail.com>
2022-09-19 09:43:03 +0900

    style : tabs의 transition 방식 변경 및 적용 #88

--- src/components/Tabs/Tabs.styled.tsx
+++ src/components/Tabs/Tabs.styled.tsx
@@ -2,10 +2,7 @@ import styled, { css } from 'styled-components';
 
 import { flexCenter } from '@styles/mixin';
 
-type WrapperPropsType = {
-  selectedTabWidth: number;
-  selectedTabLeft: number;
-};
+type WrapperPropsType = {};
 
 type TabWrapperPropsType = {
   active: boolean;
@@ -16,24 +13,11 @@ export const tabsGap = '1.25rem';
 export const tabsGapPx = 20;
 
 export const Wrapper = styled.div<WrapperPropsType>`
-  ${({ theme: { colors }, selectedTabWidth, selectedTabLeft }) => css`
-    display: flex;
-    position: relative;
-    justify-content: flex-start;
-    height: ${tabsHeight};
-    gap: ${tabsGap};
-
-    ::after {
-      position: absolute;
-      transition: 0.3s all;
-      left: ${selectedTabLeft}px;
-      border-bottom: solid 0.125rem ${colors.orange2};
-      width: ${selectedTabWidth}px;
-      height: 2.6rem;
-      content: '';
-      pointer-events: none;
-    }
-  `}
+  display: flex;
+  position: relative;
+  justify-content: flex-start;
+  height: ${tabsHeight};
+  gap: ${tabsGap};
 `;
 
 export const TabWrapper = styled.div<TabWrapperPropsType>`
@@ -46,5 +30,15 @@ export const TabWrapper = styled.div<TabWrapperPropsType>`
     height: 100%;
     transition: 0.3s ease-out;
     color: ${!active && colors.grey2};
+
+    ::after {
+      position: absolute;
+      transition: 0.3s all;
+      width: 100%;
+      border-bottom: solid 0.125rem ${active ? colors.orange2 : 'transparent'};
+      height: 2.6rem;
+      content: '';
+      pointer-events: none;
+    }
   `}
 `;
--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,5 +1,3 @@
-import { Dispatch, SetStateAction, useLayoutEffect, useRef, useState } from 'react';
-
 import { RecoilState, useSetRecoilState } from 'recoil';
 
 import * as S from '@components/Tabs/Tabs.styled';
@@ -8,7 +6,6 @@ export type TabsInfoType<T> = {
   title: string;
   value: T;
   active: boolean;
-  order: number;
 }[];
 
 type TabsPropsType<T> = {
@@ -16,69 +13,16 @@ type TabsPropsType<T> = {
   targetAtom: RecoilState<T>;
 };
 
-type TabPropsType<T> = {
-  title: string;
-  value: T;
-  active: boolean;
-  order: number;
-  targetAtom: RecoilState<T>;
-  setWidthArray: Dispatch<SetStateAction<number[]>>;
-  setSelectedTabOrder: Dispatch<SetStateAction<number>>;
-};
-
-const Tab = <T extends unknown>({
-  title,
-  value,
-  active,
-  order,
-  targetAtom,
-  setWidthArray,
-  setSelectedTabOrder,
-}: TabPropsType<T>) => {
-  const widthRef = useRef<HTMLDivElement>(null);
-  const setAtom = useSetRecoilState(targetAtom);
-
-  const handleClickTab = () => {
-    setAtom(value);
-    setSelectedTabOrder(order);
-  };
-
-  useLayoutEffect(() => {
-    const newWidth = widthRef.current?.offsetWidth;
-
-    if (!newWidth) return;
-
-    setWidthArray((widthArray) => {
-      const newWidthArray = [...widthArray];
-      newWidthArray[order] = newWidth;
-      return newWidthArray;
-    });
-  }, []);
-
-  return (
-    <S.TabWrapper ref={widthRef} key={title} onClick={handleClickTab} active={active}>
-      <span>{title}</span>
-    </S.TabWrapper>
-  );
-};
-
 /** 두 가지 이상의 카테고리를 보여줄 때 사용합니다. */
 const Tabs = <T extends unknown>({ tabsInfo, targetAtom }: TabsPropsType<T>) => {
-  const [widthArray, setWidthArray] = useState<number[]>([]);
-  const [selectedTabOrder, setSelectedTabOrder] = useState(0);
-  const selectedTabWidth = widthArray[selectedTabOrder];
-  const selectedWidthArray = widthArray.slice(0, selectedTabOrder);
-  const selectedTabLeft = selectedWidthArray.reduce((prev, cur) => prev + cur + S.tabsGapPx, 0);
-
-  const tabs = tabsInfo.map((info) => (
-    <Tab<T> key={info.title} {...{ targetAtom, setWidthArray, setSelectedTabOrder }} {...info} />
+  const setAtom = useSetRecoilState(targetAtom);
+  const tabs = tabsInfo.map(({ title, value, active }) => (
+    <S.TabWrapper key={title} onClick={() => setAtom(value)} active={active}>
+      <span>{title}</span>
+    </S.TabWrapper>
   ));
 
-  return (
-    <S.Wrapper selectedTabWidth={selectedTabWidth} selectedTabLeft={selectedTabLeft}>
-      {tabs}
-    </S.Wrapper>
-  );
+  return <S.Wrapper>{tabs}</S.Wrapper>;
 };
 
 export default Tabs;
