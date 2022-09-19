842ce6089b055f12955254e912c28d9f73b6d4dc
JinJeon <nanyong01@gmail.com>
2022-09-19 10:46:31 +0900

    feat : notice keyword 컴포넌트 기능 추가 #88

--- src/components/NoticeKeyword/index.tsx
+++ src/components/NoticeKeyword/index.tsx
@@ -1,12 +1,47 @@
+import moment from 'moment';
+import 'moment/locale/ko';
+
 import { NoticeKeywordDataType } from '@api/notice';
-import * as S from '@components/NoticeActivity/NoticeActivity.style';
+import ImgContainer from '@components/ImgContainer';
+import * as S from '@components/NoticeKeyword/NoticeKeyword.style';
+import { newShareEnrolledMention } from '@constants/mentions';
 
-type NoticeActivityPropsType = { contents: NoticeKeywordDataType }; // active notice 값에 따라 올바른 contents type 들어오도록 재설정 필요
+type NoticeKeywordPropsType = { contents: NoticeKeywordDataType }; // active notice 값에 따라 올바른 contents type 들어오도록 재설정 필요
 
-const NoticeKeyword = ({ contents }: NoticeActivityPropsType) => {
-  const Items = contents.map(({}) => <S.Item></S.Item>);
+const NoticeKeyword = ({ contents }: NoticeKeywordPropsType) => {
+  const Items = contents.map(
+    ({
+      shareLocation,
+      shareId,
+      shareThumbnailImageUrl,
+      shareTitle,
+      notificationCreatedDateTime,
+    }) => {
+      const diffTime = moment(notificationCreatedDateTime).fromNow();
 
-  return <S.Wrapper>{}</S.Wrapper>;
+      return (
+        <S.Item key={shareId}>
+          <ImgContainer
+            imgSrc={shareThumbnailImageUrl}
+            imgTitle={shareTitle}
+            imgWrapperRatio={1 / 1}
+            imgWrapperWidth={S.itemHeight}
+            borderRadius='0.25rem'
+          />
+          <S.TextWrapper>
+            <S.TextUpper>
+              <S.LocationBox>{shareLocation}</S.LocationBox>
+              <S.EnrollMention>{newShareEnrolledMention}</S.EnrollMention>
+            </S.TextUpper>
+            <S.TitleWrapper>{shareTitle}</S.TitleWrapper>
+            <S.DiffTime>{diffTime}</S.DiffTime>
+          </S.TextWrapper>
+        </S.Item>
+      );
+    },
+  );
+
+  return <S.Wrapper>{Items}</S.Wrapper>;
 };
 
 export default NoticeKeyword;
