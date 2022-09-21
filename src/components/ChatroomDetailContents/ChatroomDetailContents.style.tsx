c5eb7f72b941a37da518bb5992ab7ad7b580c995
JinJeon <nanyong01@gmail.com>
2022-09-21 10:45:49 +0900

    refactor : chatroom detail 렌더링 시 socket 연결 및 자동스크롤 리펙토링 #95

--- src/components/ChatroomDetailContents/ChatroomDetailContents.style.tsx
+++ src/components/ChatroomDetailContents/ChatroomDetailContents.style.tsx
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
-export const Wrapper = styled.div``;
+import { navigationBarHeight } from '@components/NavigationBar/NavigationBar.style';
+
+export const Wrapper = styled.div`
+  padding-bottom: 0;
+`;
 
 export const Date = styled.div`
   ${({ theme: { colors, fonts } }) => css`
@@ -17,4 +21,9 @@ export const Chats = styled.div`
   flex-direction: column;
   gap: 1.5rem;
   padding-top: 1rem;
+`;
+
+export const EmptyBlock = styled.div`
+  height: ${navigationBarHeight};
+  width: 100%;
 `;
