638351287a11e6cd70d78b6f758e7656edfd3037
JinJeon <nanyong01@gmail.com>
2022-09-21 10:38:28 +0900

    fix : 채팅방 입장 시 바로 socket이 연결되도록 수정 #95

--- src/socket/chatroomSocket.ts
+++ src/socket/chatroomSocket.ts
@@ -3,65 +3,75 @@ import { Dispatch, SetStateAction } from 'react';
 import SockJs from 'sockjs-client';
 import StompJs from 'stompjs';
 
-import { APP, CHAT, CHATROOMS, TOPIC } from '@constants/words';
+import { APP, CHAT, CHATROOMS, TOPIC, WEBSOCKET } from '@constants/words';
 import { TestChatroomDetailChatsType } from '@pages/ChatroomDetail/chatroomDetailData';
 import { getAuthHeaders } from '@utils/getAuthHeaders';
-
-const sockServer = 'http://louie-03.com/websocket'; // 들어갈 주소 설정
-const sock = new SockJs(sockServer);
-const stompClient = StompJs.over(sock);
 
 type subscribeParamsType = {
   setter: Dispatch<SetStateAction<TestChatroomDetailChatsType>>;
   chatroomId: string;
 };
 
-const subscribe = ({ setter, chatroomId }: subscribeParamsType) => {
-  const subscribeURL = `/${TOPIC}/${CHATROOMS}/${chatroomId}`;
-  const headers = getAuthHeaders();
+type SendChatParamsType = { contents: string; chatroomId?: string };
 
-  stompClient.subscribe(
-    subscribeURL,
-    (chatData) => {
-      const newChat = JSON.parse(chatData.body);
+const sockServer = `${process.env.REACT_APP_BASE_URL}/${WEBSOCKET}`; // 들어갈 주소 설정
+const sock = new SockJs(sockServer);
+const stompClient = StompJs.over(sock);
 
-      setter((chats) => {
-        const newChats = [...chats, newChat];
-
-        return newChats;
-      });
-    },
-    headers,
-  );
-};
-
-const unsubscribe = () => {
-  stompClient.unsubscribe('sub-0');
-};
-
-export const chatroomConnect = (subscribeParams: subscribeParamsType) => {
-  const headers = getAuthHeaders();
-
-  try {
-    // stompClient.debug = () => null;
-    stompClient.connect(headers, () => subscribe(subscribeParams));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const chatroomDisconnect = () => {
-  try {
-    // stompClient.debug = () => null;
-    stompClient.disconnect(() => unsubscribe());
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const sendChat = ({ contents, chatroomId }: { contents: string; chatroomId?: string }) => {
+const sendChat = ({ contents, chatroomId }: SendChatParamsType) => {
   const headers = getAuthHeaders();
   const sendingURL = `/${APP}/${CHATROOMS}/${chatroomId}/${CHAT}`;
 
   stompClient.send(sendingURL, headers, JSON.stringify({ contents }));
+};
+
+export const connectChat = () => {
+  const sockServer = `${process.env.REACT_APP_BASE_URL}/${WEBSOCKET}`; // 들어갈 주소 설정
+  const sock = new SockJs(sockServer);
+  const stompClient = StompJs.over(sock);
+
+  const subscribeChat = ({ setter, chatroomId }: subscribeParamsType) => {
+    const subscribeURL = `/${TOPIC}/${CHATROOMS}/${chatroomId}`;
+    const headers = getAuthHeaders();
+
+    stompClient.subscribe(
+      subscribeURL,
+      (chatData) => {
+        const newChat = JSON.parse(chatData.body);
+
+        setter((chats) => {
+          const newChats = [...chats, newChat];
+
+          return newChats;
+        });
+      },
+      headers,
+    );
+  };
+
+  const unsubscribe = () => {
+    stompClient.unsubscribe('sub-0');
+  };
+
+  const chatroomConnect = (subscribeParams: subscribeParamsType) => {
+    const headers = getAuthHeaders();
+
+    try {
+      // stompClient.debug = () => null;
+      stompClient.connect(headers, () => subscribeChat(subscribeParams));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const chatroomDisconnect = () => {
+    try {
+      // stompClient.debug = () => null;
+      stompClient.disconnect(() => unsubscribe());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { chatroomConnect, chatroomDisconnect, sendChat };
 };
