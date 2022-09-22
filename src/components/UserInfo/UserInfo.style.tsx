44d96aeb7882d377a3634acc8c5ac54ef9b46882
JinJeon <nanyong01@gmail.com>
2022-09-22 18:02:03 +0900

    feat : profile 페이지에서 user info를 통해 사용자 정보를 보여주도록 설정 #104

--- src/components/UserInfo/UserInfo.style.tsx
+++ src/components/UserInfo/UserInfo.style.tsx
@@ -2,40 +2,69 @@ import styled, { css } from 'styled-components';
 
 import { flexBetween } from '@styles/mixin';
 
+type SetCheckType = { isSet: boolean };
+
 export const Wrapper = styled.div`
   ${flexBetween}
   padding-top: 1rem;
 `;
 
-export const Image = styled.img`
-  float: left;
-  margin-right: 12px;
-  border-radius: 50px;
-  width: 60px;
-  height: 60px;
+export const ImgWrapper = styled.div<SetCheckType>`
+  ${({ isSet, theme: { colors } }) =>
+    !isSet &&
+    css`
+      width: 3.5rem;
+      height: 3.5rem;
+      opacity: 0.25;
+      background-color: ${colors.orange0};
+      border-radius: 10rem;
+    `}
 `;
 
 export const InfoContainer = styled.div`
   display: flex;
+  gap: 0.81rem;
 `;
 
 export const Info = styled.div`
   display: flex;
+  gap: 0.4rem;
   flex-direction: column;
-  gap: 10px;
   justify-content: center;
 `;
 
-export const Nickname = styled.h2`
-  ${({ theme: { colors } }) => css`
+export const Nickname = styled.h2<SetCheckType>`
+  ${({ isSet, theme: { colors } }) => css`
+    display: flex;
+    align-items: center;
+    height: 1rem;
     color: ${colors.white0};
+
+    ${!isSet &&
+    css`
+      width: 5rem;
+      opacity: 0.25;
+      border-radius: 0.25rem;
+      background-color: ${colors.orange0};
+    `}
   `}
 `;
 
-export const Email = styled.span`
-  ${({ theme: { fonts, colors } }) => css`
+export const Email = styled.span<SetCheckType>`
+  ${({ isSet, theme: { fonts, colors } }) => css`
+    display: flex;
+    height: 1rem;
+    align-items: center;
     ${fonts.smallRegular}
     color:${colors.white0};
+
+    ${!isSet &&
+    css`
+      width: 10rem;
+      opacity: 0.25;
+      border-radius: 0.25rem;
+      background-color: ${colors.orange0};
+    `}
   `}
 `;
 
