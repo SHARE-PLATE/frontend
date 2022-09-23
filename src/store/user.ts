7c8166f530ff604dc3a2edf451ea2b617b4e74b4
JinJeon <nanyong01@gmail.com>
2022-09-23 15:27:09 +0900

    refactor : 로그인 여부 확인 상태를 selector로 변경

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,11 +1,26 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
+
+import { checkVerification, getBackAccessToken } from '@api/account';
 
 export const thumbnailImageUrl = atom<string>({
   key: 'thumbnailImageUrl',
   default: '',
 });
 
-export const isLoginState = atom<boolean>({
-  key: 'isLogin',
-  default: false,
+export const isLoginTriggerState = atom<number>({
+  key: 'GET/verification',
+  default: 0,
+});
+
+export const isLoginState = selector<boolean>({
+  key: 'GET/isLogin',
+  get: async ({ get }) => {
+    get(isLoginTriggerState);
+    const isVerificationRight = await checkVerification();
+    if (isVerificationRight) return true;
+    const isGetAccessToken = await getBackAccessToken();
+    if (isGetAccessToken) return true;
+
+    return false;
+  },
 });
