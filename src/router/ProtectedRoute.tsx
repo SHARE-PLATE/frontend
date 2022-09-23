6ee988e7eca1b9c8cda8218c969dbe4e9025d6ef
JinJeon <nanyong01@gmail.com>
2022-09-23 15:28:49 +0900

    refactor : protected router에 selector가 적용된 화면 구성 적용

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -1,17 +1,29 @@
 import { Outlet } from 'react-router-dom';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValueLoadable, useSetRecoilState } from 'recoil';
 
 import HomeLogin from '@components/HomeLogin';
+import Loading from '@components/Loading';
 import { portalState } from '@store/portal';
 import { isLoginState } from '@store/user';
 
 const ProtectedRoute = () => {
-  const isLogin = useRecoilValue(isLoginState);
+  const { state, contents } = useRecoilValueLoadable(isLoginState);
   const setPortal = useSetRecoilState(portalState);
 
-  if (!isLogin) setPortal('login');
+  const getContent = () => {
+    switch (state) {
+      case 'hasValue':
+        if (!contents) setPortal('login');
+        return contents ? <Outlet /> : <HomeLogin />;
+      case 'hasError':
+        setPortal('login');
+        return <HomeLogin />;
+      case 'loading':
+        return <Loading color='grey1' size={42} border={6} height='100vh' />;
+    }
+  };
 
-  return isLogin ? <Outlet /> : <HomeLogin />;
+  return getContent();
 };
 
 export default ProtectedRoute;
