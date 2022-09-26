bcd33b5ed010dfdc23c1df25943e2da2222abdd4
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-09-26 16:18:27 +0900

    feat : 파라미터에 따라 이미지 위 컨텐츠 보여주기 #108

---
+++ src/components/common/ImageContents/index.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+
+import ExpirationDate from '@components/ExpirationDate';
+import { RemainedTime } from '@components/RemainedTime';
+import WishHeart from '@components/WishHeart';
+
+interface ImageContentsPropsType {
+  dateTime: string;
+  isDone?: boolean;
+  isWish?: boolean;
+  wishListClickHandler?: () => void;
+}
+
+const ImageContents = ({
+  isDone,
+  isWish,
+  dateTime,
+  wishListClickHandler,
+}: ImageContentsPropsType) => {
+  const [currentKey, setCurrentKey] = useState<string>('');
+
+  useEffect(() => {
+    if (isDone && isWish) return setCurrentKey('done_wish');
+    if (isDone) return setCurrentKey('done');
+    if (isWish) return setCurrentKey('wish');
+    setCurrentKey('');
+  }, [isDone, isWish]);
+
+  switch (currentKey) {
+    case 'done_wish':
+      return (
+        <>
+          <WishHeart type='delivery' clickHandler={wishListClickHandler} />
+          <ExpirationDate />
+        </>
+      );
+
+    case 'done':
+      return <ExpirationDate />;
+
+    case 'wish':
+      return (
+        <>
+          <WishHeart type='delivery' clickHandler={wishListClickHandler} />
+          <RemainedTime targetTime={dateTime} position={{ top: '0.375rem', left: '0.375rem' }} />
+        </>
+      );
+
+    default:
+      return (
+        <RemainedTime targetTime={dateTime} position={{ top: '0.375rem', left: '0.375rem' }} />
+      );
+  }
+};
+
+export default ImageContents;
