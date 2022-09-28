fd59cfc34f7ceb715035386bedf2053c32a2e225
JinJeon <nanyong01@gmail.com>
2022-09-28 13:36:12 +0900

    feat : half image item에 클릭 시 해당 링크로 이동하도록 설정 #107

--- src/components/ShareListItemHalfImage/index.tsx
+++ src/components/ShareListItemHalfImage/index.tsx
@@ -1,29 +1,39 @@
+import { useNavigate } from 'react-router-dom';
+
 import * as S from '@components/ShareListItemHalfImage/ShareListItemHalfImage.style';
 import ImgContainer from '@components/common/ImgContainer';
-import { ShareListType } from '@type/shareList';
+import PersonnelStatus from '@components/common/PersonnelStatus';
+import { pathName } from '@constants/pathName';
+import { ShareRecommendationType } from '@type/shareList';
 import { getPriceType } from '@utils/getPriceType';
 
 type ShareListItemHalfImagePropsType = {
-  itemInfo?: ShareListType;
+  itemInfo?: ShareRecommendationType;
 };
 
 const ShareListItemHalfImage = ({ itemInfo }: ShareListItemHalfImagePropsType) => {
-  if (!itemInfo) return <S.Wrapper />;
+  if (!itemInfo) return <S.Wrapper isEmpty={true} />;
 
-  const { thumbnailUrl, title, price } = itemInfo;
+  const navigate = useNavigate();
+  const { thumbnailUrl, title, price, currentRecruitment, finalRecruitment, id } = itemInfo;
+
+  const handleClick = () => {
+    navigate(`${pathName.shareDetail}/${id}`);
+  };
 
   return (
-    <S.Wrapper>
+    <S.Wrapper onClick={handleClick}>
       <ImgContainer
         imgSrc={thumbnailUrl}
         imgTitle={title}
         imgWrapperWidth='100%'
-        imgWrapperRatio={1.8 / 1}
+        imgWrapperRatio={1.44 / 1}
         borderRadius='0.5rem'
       />
       <S.InfoWrapper>
         <div>{title}</div>
         <div>{getPriceType({ price, isUnit: true })}</div>
+        <PersonnelStatus curPersonnel={currentRecruitment} totalPersonnel={finalRecruitment} />
       </S.InfoWrapper>
     </S.Wrapper>
   );
