eae4977a33ae436314e944037328cb06820f7a88
JinJeon <nanyong01@gmail.com>
2022-09-28 16:43:17 +0900

    refactor : chat 관련 api 타입 재설정 및 api 리펙토링

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -5,19 +5,68 @@ import { CHATROOM_ID } from '@constants/words';
 import { getAuthHeaders } from '@utils/getAuthHeaders';
 
 export type GetChatroomsDataParamsType = {
-  id?: string;
-  type?: 'entry' | 'question';
+  type: 'entry' | 'question';
 };
 
-export const getChatroomsData = async ({ id, type }: GetChatroomsDataParamsType) => {
+export type GetChatroomDetailDataParamsType = {
+  id: string;
+};
+
+export type ChatroomsDataType = {
+  id: string;
+  chatRoomMemberId: number;
+  shareThumbnailImageUrl: string;
+  currentRecruitment: number;
+  recentMessage: string;
+  recentMessageDataTime: string;
+  recruitmentMemberNicknames: string[];
+  recruitmentMemberImageUrls: string[];
+  unreadCount: number;
+};
+
+export type ChatroomDetailDataType = {
+  chatRoomMemberId: string;
+  share: {
+    id: number;
+    thumbnailImageUrl: string;
+    title: string;
+    price: number;
+    originalPrice: number;
+    currentRecruitment: number;
+    finalRecruitment: number;
+    cancel: boolean;
+  };
+  chats: {
+    contents: string;
+    writer: string;
+    writerThumbnailImageUrl: string;
+    writtenDateTime: string;
+    writtenByMe: boolean;
+  }[];
+};
+
+export const getChatroomsData = async ({ type }: GetChatroomsDataParamsType) => {
   const headers = getAuthHeaders();
 
   try {
-    const response = await axios.get(`${API.CHATROOMS}/${id || ''}`, {
+    const response = await axios.get<ChatroomsDataType[]>(API.CHATROOMS, {
       headers,
       params: {
         type,
       },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getChatroomDetailData = async ({ id }: GetChatroomDetailDataParamsType) => {
+  const headers = getAuthHeaders();
+
+  try {
+    const response = await axios.get<ChatroomDetailDataType>(`${API.CHATROOMS}/${id}`, {
+      headers,
     });
     return response.data;
   } catch (error) {
