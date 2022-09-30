d67758f9979f7315ba52b2e451df3637cd953546
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-09-30 15:19:34 +0900

    style : css 클릭 막기 #115

--- src/components/ExpirationDate/ExpirationDate.style.ts
+++ src/components/ExpirationDate/ExpirationDate.style.ts
@@ -1,22 +1,23 @@
 import styled, { css } from 'styled-components';
 
+import { flexCenter } from '@styles/mixin';
+
 export const ExpirationDateWrapper = styled.div`
   ${({ theme: { colors, fonts } }) => css`
     ${fonts.xSmallBold}
-    display: flex;
+    ${flexCenter}
+    background-color: ${colors.black};
+    color: ${colors.white1};
     flex-direction: column;
-    justify-content: center;
-    text-align: center;
     gap: 4px;
     position: absolute;
     top: 0px;
     left: 0px;
     border-radius: 0.4rem;
-    background-color: ${colors.black};
     opacity: 0.6;
     width: 100%;
     height: 100%;
-    color: ${colors.white1};
+    pointer-events: none;
   `}
 `;
 
