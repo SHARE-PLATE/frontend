286c5e7592d69ebb059407a569c0976d85467c66
JinJeon <nanyong01@gmail.com>
2022-10-04 19:26:08 +0900

    fix : option portal button 클릭 시 쉐어 작성 submit 이벤트 차단

--- src/components/ShareForm/OptionPortalButton/index.tsx
+++ src/components/ShareForm/OptionPortalButton/index.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from 'react';
+
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { OptionBtn } from '@pages/ShareRegistration/ShareRegistration.style';
@@ -8,8 +10,13 @@ const OptionPortalButton = () => {
   const setPortal = useSetRecoilState(portalState);
   const isSelectedOptionStateValue = useRecoilValue(isSelectedOptionState);
 
+  const handleClickButton = (event: MouseEvent) => {
+    event.preventDefault();
+    setPortal('option');
+  };
+
   return (
-    <OptionBtn onClick={() => setPortal('option')} isOption={isSelectedOptionStateValue}>
+    <OptionBtn onClick={handleClickButton} isOption={isSelectedOptionStateValue}>
       <span>옵션선택</span>
     </OptionBtn>
   );
