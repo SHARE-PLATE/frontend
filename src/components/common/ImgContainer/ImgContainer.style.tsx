9b98eef2cee90c79e4cb059aaffb6c408243f0a8
JinJeon <nanyong01@gmail.com>
2022-10-10 18:46:04 +0900

    style : 이미지 로딩 완료 후 이미지가 서서히 보이도록 수정

--- src/components/common/ImgContainer/ImgContainer.style.tsx
+++ src/components/common/ImgContainer/ImgContainer.style.tsx
@@ -16,13 +16,19 @@ export const Wrapper = styled.div<ImgWrapperPropsType>`
     aspect-ratio: ${P.imgWrapperRatio};
     border-radius: ${P.borderRadius};
     overflow: hidden;
-    opacity: 1;
+    opacity: 0;
+    transition: all 0.15s;
 
     img {
       width: 100%;
       height: 100%;
       object-fit: cover;
     }
+
+    ${P.isLoaded &&
+    css`
+      opacity: 1;
+    `}
 
     ${P.additionalStyle};
   `}
