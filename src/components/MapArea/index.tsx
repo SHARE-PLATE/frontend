7b1d2c44ab74425157b70abb1e0e5386d2011028
JinJeon <nanyong01@gmail.com>
2022-10-10 17:15:35 +0900

    refactor : map area 내 불필요한 요소 제거 및 리펙토링

--- src/components/MapArea/index.tsx
+++ src/components/MapArea/index.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useRef, useState, useLayoutEffect, MouseEvent, TouchEvent } from 'react';
-
-import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { useEffect, useRef, useState } from 'react';
 
 import { CurrentLocationMarker } from '@components/MapArea/CurrentLocationMarker';
 import * as S from '@components/MapArea/MapArea.style';
 import { ShareListMarker } from '@components/MapArea/ShareListMarker';
-import { curTopAtom, maxTopAtom, minTopAtom } from '@store/shareMap';
 import { ShareListType } from '@type/shareList';
 
 interface MapAreaPropsType {
@@ -26,43 +23,6 @@ const MapArea = ({ lat, lng, data }: MapAreaPropsType) => {
       position: new kakao.maps.LatLng(latitude, longitude),
     };
   });
-
-  const setCurTop = useSetRecoilState(curTopAtom);
-  const minTop = useRecoilValue(minTopAtom);
-  const [totalHeight, setTotalHeight] = useState<number>(0);
-  const maxTop = useRecoilValue(maxTopAtom);
-  const [isClicked, setIsClicked] = useState<boolean>(false);
-
-  const changeClickTrue = () => setIsClicked(true);
-  const changeClickFalse = () => setIsClicked(false);
-
-  useLayoutEffect(() => {
-    const height = document.getElementsByClassName('App')[0].clientHeight;
-    setTotalHeight(height);
-  }, []);
-
-  const checkMousePoint = (e: MouseEvent<HTMLDivElement>) => {
-    if (!isClicked) return;
-
-    const y = e.clientY;
-    const tempTop = (y / totalHeight) * 100;
-    const divMousePoint = tempTop > minTop ? minTop : tempTop;
-    if (maxTop > divMousePoint) return changeClickFalse();
-
-    setCurTop(divMousePoint);
-  };
-
-  const checkTouchPoint = (e: TouchEvent<HTMLDivElement>) => {
-    if (!isClicked) return;
-
-    const y = e.touches[0].clientY;
-    const tempTop = (y / totalHeight) * 100;
-    const divMousePoint = tempTop > minTop ? minTop : tempTop;
-
-    if (maxTop > divMousePoint) return changeClickFalse();
-
-    setCurTop(divMousePoint);
-  };
 
   const initMap = () => {
     const options = { center: curLocationPosition, level: 4 };
@@ -95,10 +55,8 @@ const MapArea = ({ lat, lng, data }: MapAreaPropsType) => {
   }, [mapState]);
 
   return (
-    <S.Wrapper onMouseMove={checkMousePoint} onTouchMove={checkTouchPoint}>
-      <S.MapContainer>
-        <S.Map ref={mapRef} />
-      </S.MapContainer>
+    <S.Wrapper>
+      <S.Map ref={mapRef} />
     </S.Wrapper>
   );
 };
