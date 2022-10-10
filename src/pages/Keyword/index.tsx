793fba1857537ed04e23028e024b857b299cbee4
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-10-10 11:58:13 +0900

    feat : 텍스트 상수화 #121

--- src/pages/Keyword/index.tsx
+++ src/pages/Keyword/index.tsx
@@ -1,12 +1,13 @@
 import KeywordBox from '@components/KeywordBox';
 import BackTitleHeader from '@components/common/BackTitleHeader';
+import { ADDRESS_KEYWORD, NOTICE_KEYWORD } from '@constants/words';
 import * as S from '@pages/Keyword/Keyword.style';
 
 const Keyword = () => {
   return (
     <S.Wrapper>
-      <BackTitleHeader title='키워드 알림' />
-      <S.Header>주소 키워드</S.Header>
+      <BackTitleHeader title={NOTICE_KEYWORD} />
+      <S.Header>{ADDRESS_KEYWORD}</S.Header>
       <KeywordBox />
     </S.Wrapper>
   );
