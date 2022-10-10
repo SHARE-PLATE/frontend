34b84f2ad142101745d87c10074f9f264e9fd0cd
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-10-10 12:21:41 +0900

    feat : 회원정보 설정 페이지 레이아웃 #121

---
+++ src/pages/SettingProfile/index.tsx
@@ -0,0 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+
+import UserInfo from '@components/UserInfo';
+import UserMenu from '@components/UserMenu';
+import BackTitleHeader from '@components/common/BackTitleHeader';
+import { settingMenu } from '@constants/userMenu';
+import { EDIT_PROFILE, OPTION_PROFILE } from '@constants/words';
+import * as S from '@pages/SettingProfile/SettingProfile.style';
+
+const SettingProfile = () => {
+  const navigate = useNavigate();
+
+  return (
+    <S.Wrapper>
+      <S.Header>
+        <BackTitleHeader title={OPTION_PROFILE} />
+        <UserInfo textColor='#000' arrowIcon={false} />
+        <S.ProfileEditBtn onClick={() => navigate('./edit-user-info')}>
+          {EDIT_PROFILE}
+        </S.ProfileEditBtn>
+      </S.Header>
+      <S.Contents>
+        <UserMenu userMenu={settingMenu} />
+      </S.Contents>
+    </S.Wrapper>
+  );
+};
+
+export default SettingProfile;
