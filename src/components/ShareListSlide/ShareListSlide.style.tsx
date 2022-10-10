248853cf4852382235ec0cd3a7fe998175db0b1a
JinJeon <nanyong01@gmail.com>
2022-10-10 17:14:52 +0900

    refactor : share list slide의 애니메이션 방식 수정 및 적용

--- src/components/ShareListSlide/ShareListSlide.style.tsx
+++ src/components/ShareListSlide/ShareListSlide.style.tsx
@@ -2,38 +2,62 @@ import styled, { css } from 'styled-components';
 
 import { noScrollBar } from '@styles/mixin';
 
-export const Wrapper = styled.div<{ curTop: number }>`
-  ${({ theme: { defaultPadding, colors }, curTop }) => css`
-    ${defaultPadding};
+const slidePositionHeight = {
+  // icon wrapper height + title height = 68px
+  bottom: '68px',
+  middle: '50%',
+  top: '100%',
+};
+
+export type SlidePositionType = keyof typeof slidePositionHeight;
+
+export const Wrapper = styled.div<{ slidePositionType: SlidePositionType }>`
+  ${({ theme: { colors }, slidePositionType }) => css`
+    ${noScrollBar};
+
+    touch-action: none;
+    z-index: 100;
+    transition: all 0.3s;
     padding-bottom: 1.5rem;
     background-color: ${colors.white1};
     display: flex;
     flex-direction: column;
     position: absolute;
-    top: ${curTop}%;
-    z-index: 100;
-    height: 100%;
-    min-height: 4.375rem;
-    overflow-y: scroll;
-    scroll-behavior: smooth;
-    ${noScrollBar}
+    top: calc(100% - ${slidePositionHeight[slidePositionType]});
     box-shadow: 0px -6px 12px rgba(0, 0, 0, 0.08);
     border-radius: 8px 8px 0px 0px;
+    overflow-y: scroll;
+    scroll-behavior: smooth;
+    height: ${slidePositionHeight[slidePositionType]};
   `}
 `;
 
-export const IconWrapper = styled.div`
-  padding-top: 0.75rem;
+// set height 30spx
+export const IconWrapper = styled.div<{ isActive: boolean }>`
+  ${({ theme: { colors }, isActive }) => css`
+    background-color: ${colors.white1};
+    padding: 12px 0;
+    top: 0;
+    z-index: 2;
+    position: sticky;
+    transition: 0.3s all;
+
+    rect {
+      fill: ${isActive ? colors.grey3 : colors.orange2};
+    }
+  `}
 `;
 
+// set height 38px
 export const Title = styled.h1`
-  ${({ theme }) => theme.fonts.largeBold}
-  line-height: 1.5rem;
-  padding: 0.5rem 0 1.375rem 0;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
+  ${({ theme: { fonts, defaultPadding } }) => css`
+    ${fonts.largeBold}
+    ${defaultPadding}
+
+    padding-bottom: 22px;
+  `}
 `;
 
-export const ListContent = styled.div``;
+export const ListContent = styled.div`
+  ${({ theme: { defaultPadding } }) => defaultPadding};
+`;
--- src/components/ShareListSlide/index.tsx
+++ src/components/ShareListSlide/index.tsx
@@ -1,19 +1,23 @@
+import { MouseEvent, SetStateAction, TouchEvent, useRef } from 'react';
+import { useState } from 'react';
+import { Dispatch } from 'react';
+import { useEffect } from 'react';
+
 import { useRecoilValue } from 'recoil';
 
 import PreviewShareListBigSizeImage from '@components/PreviewShareListBigSizeImage';
 import PreviewShareListLeftImage from '@components/PreviewShareListLeftImage';
 import * as S from '@components/ShareListSlide/ShareListSlide.style';
 import Icon from '@components/common/Icon';
+import { AROUND_SHARE_LIST } from '@constants/words';
 import { activeShareList } from '@store/filterShareList';
-import { curTopAtom } from '@store/shareMap';
 import { ShareListType } from '@type/shareList';
 import { getSortData } from '@utils/ShareListSort';
 
 interface ShareListSlidePropsType {
-  isClicked?: boolean;
-  contents: ShareListType[];
-  changeClickTrue?: () => void;
-  changeClickFalse?: () => void;
+  data: ShareListType[];
+  setIsActive: Dispatch<SetStateAction<boolean>>;
+  isActive: boolean;
 }
 
 const ListContentComponentInfo = {
@@ -21,34 +25,88 @@ const ListContentComponentInfo = {
   ingredient: PreviewShareListLeftImage,
 };
 
-const ShareListSlide = ({
-  isClicked,
-  contents,
-  changeClickTrue,
-  changeClickFalse,
-}: ShareListSlidePropsType) => {
+const ShareListSlide = ({ isActive, data, setIsActive }: ShareListSlidePropsType) => {
   const activeShareListValue = useRecoilValue(activeShareList);
   const ListContentComponent = ListContentComponentInfo[activeShareListValue];
-  const curTop = useRecoilValue(curTopAtom);
+  const [slidePosition, setSlidePosition] = useState<S.SlidePositionType>('bottom');
+  const mouseMovingRef = useRef(false);
+  const targetRef = useRef(0);
+  const titleRef = useRef<HTMLDivElement>(null);
+
+  const handleTouchStart = (event: TouchEvent<HTMLDivElement>) => {
+    const { clientY } = event.touches[0];
+    if (!targetRef.current) targetRef.current = clientY;
+  };
+
+  const handleTouchMove = (event: TouchEvent<HTMLDivElement>) => {
+    const { clientY } = event.touches[0];
+    const targetClientY = targetRef.current;
+    const distanceDiff = targetClientY - clientY;
+
+    if (distanceDiff > 150) setSlidePosition('middle');
+    if (distanceDiff > 300) setSlidePosition('top');
+    if (distanceDiff < 50) setSlidePosition('bottom');
+  };
+
+  const handleMouseDown = ({ clientY }: MouseEvent<HTMLDivElement>) => {
+    targetRef.current = clientY;
+    mouseMovingRef.current = true;
+  };
+
+  const handleMouseMove = ({ clientY }: MouseEvent<HTMLDivElement>) => {
+    if (!mouseMovingRef.current) return;
+
+    const targetClientY = targetRef.current;
+    const distanceDiff = targetClientY - clientY;
+
+    if (distanceDiff > 0 && slidePosition === 'bottom') setSlidePosition('middle');
+    if (distanceDiff > 0 && slidePosition === 'middle') setSlidePosition('top');
+    if (distanceDiff < 0 && slidePosition === 'top') setSlidePosition('middle');
+    if (distanceDiff < 0 && slidePosition === 'middle') setSlidePosition('bottom');
+    if (!!distanceDiff) mouseMovingRef.current = false;
+  };
+
+  const handleMouseOut = () => {
+    mouseMovingRef.current = false;
+    targetRef.current = 0;
+  };
+
+  const handleClick = () => {
+    if (slidePosition === 'bottom') setSlidePosition('middle');
+    if (slidePosition === 'middle') setSlidePosition('top');
+    if (slidePosition === 'top') setSlidePosition('bottom');
+  };
+
+  useEffect(() => {
+    if (slidePosition === 'bottom') {
+      setIsActive(false);
+      titleRef.current?.scrollIntoView({ block: 'end' });
+    }
+    if (slidePosition === 'middle' || slidePosition === 'top') setIsActive(true);
+  }, [slidePosition]);
+
+  useEffect(() => {
+    if (isActive && slidePosition === 'bottom') setSlidePosition('top');
+    if (!isActive) setSlidePosition('bottom');
+  }, [isActive]);
 
   return (
-    <S.Wrapper
-      onMouseDown={changeClickTrue}
-      onMouseUp={changeClickFalse}
-      onTouchStart={changeClickTrue}
-      onTouchEnd={changeClickFalse}
-      curTop={curTop}
-    >
-      <S.IconWrapper>
-        {isClicked ? (
-          <Icon iconName='OnClickBar' iconSize={2.5} />
-        ) : (
-          <Icon iconName='OffClickBar' iconSize={2.5} />
-        )}
+    <S.Wrapper slidePositionType={slidePosition}>
+      <S.IconWrapper
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseOut}
+        onMouseLeave={handleMouseOut}
+        onClick={handleClick}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        isActive={isActive}
+      >
+        <Icon iconName={'OnClickBar'} iconSize={2.5} />
       </S.IconWrapper>
-      <S.Title>주변 쉐어 목록</S.Title>
+      <S.Title ref={titleRef}>{AROUND_SHARE_LIST}</S.Title>
       <S.ListContent>
-        <ListContentComponent data={getSortData('recency', contents)} />
+        <ListContentComponent data={getSortData('recency', data)} />
       </S.ListContent>
     </S.Wrapper>
   );
