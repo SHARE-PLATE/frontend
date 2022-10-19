de8f1c232090efc285dad41d7f4de69f3fbdc52b
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-10-20 02:46:47 +0900

    feat : 페이지 하단 버튼 추가 및 클릭 핸들러 구현 #130

--- src/pages/ShareSearch/ShareSearch.style.ts
+++ src/pages/ShareSearch/ShareSearch.style.ts
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-import { defaultPageStyle } from '@styles/mixin';
+import { defaultPageStyle, flexCenter } from '@styles/mixin';
 
 export const Wrapper = styled.div`
   ${defaultPageStyle}
@@ -24,4 +24,34 @@ export const ListHeader = styled.header`
 
 export const ListContent = styled.div`
   margin-top: 120px;
+`;
+
+export const AddKeywordButton = styled.button`
+  ${flexCenter}
+  flex-direction: row;
+  gap: 4px;
+  position: fixed;
+  bottom: 1.5rem;
+  left: 50%;
+  transform: translate(-50%);
+  width: 10.75rem;
+  /* width: 48%; */
+  height: 2.625rem;
+  background: ${({ theme }) => theme.colors.orange2};
+  box-shadow: 2px 4px 8px rgba(255, 69, 58, 0.2);
+  border-radius: 1.25rem;
+`;
+
+export const KeywordContent = styled.span`
+  ${({ theme: { fonts, colors } }) => css`
+    margin-left: 0.25rem;
+    ${fonts.largeBold};
+    color: ${colors.white0};
+  `}
+`;
+export const Text = styled.span`
+  ${({ theme: { fonts, colors } }) => css`
+    ${fonts.largeRegular};
+    color: ${colors.white0};
+  `}
 `;
--- src/pages/ShareSearch/index.tsx
+++ src/pages/ShareSearch/index.tsx
@@ -1,32 +1,69 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
+import { addKeywords } from '@api/keyword';
 import { getShareListData } from '@api/shareList';
 import CategoryButton from '@components/CategoryButton';
 import FailedContents from '@components/FailedContents';
 import PreviewShareListLeftImage from '@components/PreviewShareListLeftImage';
 import ShareSearchHeader from '@components/ShareSearchHeader';
+import FailedModal from '@components/common/FailedModal';
+import Icon from '@components/common/Icon';
 import { shareListCategoryItem } from '@constants/category';
+import { keywordLengthFailed } from '@constants/mentions';
+import useModal from '@hooks/useModal';
 import * as S from '@pages/ShareSearch/ShareSearch.style';
 import { currentFilterShareList } from '@store/filterShareList';
+import { keywordListTrigger, registeredKeywordLength } from '@store/keyword';
 import { currentMapKey, searchRecent } from '@store/localStorage';
-import { currentLatitudeLongitude } from '@store/location';
+import { currentLatitudeLongitude, currentAddressName } from '@store/location';
 import { ShareListType } from '@type/shareList';
 import { getSortData } from '@utils/ShareListSort';
 
 const ShareSearch = () => {
-  const curMapKey = useRecoilValue(currentMapKey);
   const [curShareFilterList, setCurrentFilterShareList] = useRecoilState(currentFilterShareList);
+  const curMapKey = useRecoilValue(currentMapKey);
+  const keywordLength = useRecoilValue(registeredKeywordLength);
   const searchRecentMapList = useRecoilValue(searchRecent);
-  const searchRecentValue = searchRecentMapList.get(curMapKey)?.name;
   const { lat, lng } = useRecoilValue(currentLatitudeLongitude);
+  const setKeywordListTrigger = useSetRecoilState(keywordListTrigger);
+  const curAddressNameArr = useRecoilValue(currentAddressName).split(' ');
+
+  const modalRef = useRef<HTMLDivElement>(null);
+  const [isKeywordLengthModalOpen, setIsKeywordLengthModalOpen] = useModal({ modalRef });
+
+  const searchRecentValue = searchRecentMapList.get(curMapKey)?.name;
   const [searchData, setSearchData] = useState<ShareListType[]>();
 
   const getShareList = async () => {
     const location = { lat, lng };
     const shareListData = await getShareListData({ location, keyword: searchRecentValue });
     setSearchData(shareListData);
+  };
+
+  const handleSubmitClick = async () => {
+    if (keywordLength >= 10) {
+      setIsKeywordLengthModalOpen(true);
+      return false;
+    }
+
+    let regionName;
+    if (curAddressNameArr.length <= 4) regionName = curAddressNameArr[2];
+    else regionName = curAddressNameArr[3];
+
+    const newKeyword = {
+      location: regionName,
+      latitude: String(lat),
+      longitude: String(lng),
+      contents: searchRecentValue || '',
+    };
+
+    const isSuccessFetch = await addKeywords(newKeyword);
+
+    if (isSuccessFetch) {
+      setKeywordListTrigger((prev) => prev + 1);
+    }
   };
 
   useEffect(() => {
@@ -42,6 +79,19 @@ const ShareSearch = () => {
           setCurrentFilterList={setCurrentFilterShareList}
         />
       </S.ListHeader>
+      <S.AddKeywordButton onClick={handleSubmitClick}>
+        <Icon iconName='NoticeFill' />
+        <S.KeywordContent>{searchRecentValue}</S.KeywordContent>
+        <S.Text> 알림 받기</S.Text>
+      </S.AddKeywordButton>
+      {isKeywordLengthModalOpen && (
+        <FailedModal
+          modalRef={modalRef}
+          closeModal={() => setIsKeywordLengthModalOpen(false)}
+          text={keywordLengthFailed}
+        />
+      )}
+
       {searchData?.length ? (
         <S.ListContent>
           <PreviewShareListLeftImage data={getSortData(curShareFilterList, searchData)} />
