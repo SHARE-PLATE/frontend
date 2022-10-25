feb860f035159522844a81e2c7698a6f3422f3ce
JinJeon <nanyong01@gmail.com>
2022-10-25 14:36:57 +0900

    style : chat unread 컴포넌트 스타일 수정

--- src/components/ChatsUnread/index.tsx
+++ src/components/ChatsUnread/index.tsx
@@ -11,7 +11,7 @@ const ChatsUnread = ({ state, newCount }: ChatsUnreadPropsType) => {
   const [count, setCount] = useState<number | null>(0);
   if (newCount !== count && state === 'hasValue') setCount(newCount);
 
-  return <S.Wrapper isShowed={!!count}>{count}</S.Wrapper>;
+  return <S.Wrapper isShowed={!!count}>{count && count > 99 ? '99+' : count}</S.Wrapper>;
 };
 
 export default ChatsUnread;
