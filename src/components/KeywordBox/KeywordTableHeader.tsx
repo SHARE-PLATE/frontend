b18b9c0d44a0bcc04c4a3d67d380f1a025c27e03
JinJeon <nanyong01@gmail.com>
2022-11-02 01:59:00 +0900

    refactor : keyword 페이지 내에 select modal 변경 사항 임시 적용

--- src/components/KeywordBox/KeywordTableHeader.tsx
+++ src/components/KeywordBox/KeywordTableHeader.tsx
@@ -1,28 +1,46 @@
-import { Dispatch, MouseEvent } from 'react';
+import { MouseEvent } from 'react';
 
+import { useSetRecoilState } from 'recoil';
+
+import { deleteKeywordAddress } from '@api/keyword';
 import * as S from '@components/KeywordBox/KeywordBox.style';
 import Icon from '@components/common/Icon';
+import { addressKeywordQuestionMention } from '@constants/mentions';
+import { DELETE } from '@constants/words';
+import { keywordListTrigger } from '@store/keyword';
+import { selectModalInfoState } from '@store/modal';
 
 interface KeywordTableHeaderPropsType {
   location: string;
-  openModal: (e: MouseEvent) => void;
-  setClickedLocation: Dispatch<React.SetStateAction<string>>;
 }
 
-const KeywordTableHeader = ({
-  location,
-  openModal,
-  setClickedLocation,
-}: KeywordTableHeaderPropsType) => {
-  const deleteClickHandler = (e: MouseEvent) => {
-    setClickedLocation(location);
-    openModal(e);
+const KeywordTableHeader = ({ location }: KeywordTableHeaderPropsType) => {
+  const setSelectModalInfo = useSetRecoilState(selectModalInfoState);
+  const setKeywordListTrigger = useSetRecoilState(keywordListTrigger);
+
+  const onClickDeleteKeywordButton = async () => {
+    const isSuccessFetch = await deleteKeywordAddress(location);
+
+    if (isSuccessFetch) {
+      setKeywordListTrigger((prev) => prev + 1);
+    }
+  };
+
+  const handleClickDeleteIcon = (event: MouseEvent) => {
+    event.stopPropagation();
+
+    setSelectModalInfo(({ trigger }) => ({
+      trigger: trigger + 1,
+      onClickOkButton: onClickDeleteKeywordButton,
+      okMention: DELETE,
+      answeringMention: addressKeywordQuestionMention,
+    }));
   };
 
   return (
     <S.Header>
       <S.Title>{location}</S.Title>
-      <Icon iconName='DeleteCircle' handleClick={(e) => deleteClickHandler(e)} />
+      <Icon iconName='DeleteCircle' handleClick={handleClickDeleteIcon} />
     </S.Header>
   );
 };
--- src/components/KeywordBox/index.tsx
+++ src/components/KeywordBox/index.tsx
@@ -1,43 +1,16 @@
-import { useRef, useState, MouseEvent } from 'react';
-
 import { useNavigate } from 'react-router-dom';
-import { useRecoilValueLoadable, useSetRecoilState } from 'recoil';
+import { useRecoilValueLoadable } from 'recoil';
 
-import { deleteKeywordAddress } from '@api/keyword';
 import * as S from '@components/KeywordBox/KeywordBox.style';
 import KeywordTable from '@components/KeywordBox/KeywordTable';
 import KeywordTableHeader from '@components/KeywordBox/KeywordTableHeader';
-import SelectModal from '@components/common/SelectModal';
-import { addressKeywordQuestionMention } from '@constants/mentions';
 import { pathName } from '@constants/pathName';
-import { DELETE } from '@constants/words';
-import useModal from '@hooks/useModal';
-import { getKeywordListsData, keywordListTrigger } from '@store/keyword';
+import { getKeywordListsData } from '@store/keyword';
 import { keywordDataType } from '@type/keyword';
 
 const KeywordBox = () => {
-  const modalRef = useRef<HTMLDivElement>(null);
   const navigate = useNavigate();
-  const [isDeleteModal, setIsDeleteModal] = useModal({ modalRef });
-  const [clickedLocation, setClickedLocation] = useState<string>('');
   const { state, contents } = useRecoilValueLoadable(getKeywordListsData);
-  const setKeywordListTrigger = useSetRecoilState(keywordListTrigger);
-
-  const closeModal = () => setIsDeleteModal(false);
-  const openModal = (e: MouseEvent) => {
-    e.stopPropagation();
-    setIsDeleteModal(true);
-  };
-
-  const deleteHandler = async (parameter: string) => {
-    if (!parameter) return false;
-    const isSuccessFetch = await deleteKeywordAddress(parameter);
-
-    if (isSuccessFetch) {
-      closeModal();
-      setKeywordListTrigger((prev) => prev + 1);
-    }
-  };
 
   const keywordTableClickHandler = (location: string) =>
     navigate(pathName.addKeyword, {
@@ -55,23 +28,10 @@ const KeywordBox = () => {
                 keywordTableClickHandler(location);
               }}
             >
-              <KeywordTableHeader
-                location={location}
-                openModal={openModal}
-                setClickedLocation={setClickedLocation}
-              />
+              <KeywordTableHeader location={location} />
               <KeywordTable keywords={keywords} />
             </S.TableBox>
           ))}
-          {isDeleteModal && (
-            <SelectModal
-              modalRef={modalRef}
-              closeModal={closeModal}
-              onClickOkButton={() => deleteHandler(clickedLocation)}
-              answeringMention={addressKeywordQuestionMention}
-              okMention={DELETE}
-            />
-          )}
         </S.Table>
       );
     case 'loading':
