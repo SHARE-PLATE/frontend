8258a8182dc2aafd1fc9e1b3453e286e0d6e673c
JinJeon <nanyong01@gmail.com>
2022-12-10 14:21:28 +0900

    refactor : 소켓 연결 재시도를 위한 atom 생성

--- src/store/socket.ts
+++ src/store/socket.ts
@@ -1,6 +1,14 @@
 import { atom } from 'recoil';
 
+export const retryConnectSocketInterval = 5000; // ms
+export const retryConnectSocketTimeMax = 10 * 60 * 1000; // 10mins
+
 export const socketConnectState = atom<boolean>({
   key: 'socketConnect',
   default: false,
+});
+
+export const retryConenctSocketTimeState = atom<number>({
+  key: 'retryConenctSocketTimeState',
+  default: 0,
 });
